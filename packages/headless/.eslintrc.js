4feb9a0c54419fd6793aac047c0d656b78db9837
Clay Griffiths <clay@claygriffiths.com>
2020-11-19 13:18:54 -0600

    🚨 fix ESLint for headless package

--- packages/headless/.eslintrc.js
+++ packages/headless/.eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
         ecmaFeatures: {
             ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
             sourceType: 'module', // Allows for the use of imports
