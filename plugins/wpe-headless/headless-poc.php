2d0e30abc7c067d1ced56a1617b1719fe00b905f
Ryan Meier <ryan.meier@wpengine.com>
2020-11-24 14:22:07 -0600

    Initial plugin merge

--- plugins/wpe-headless/headless-poc.php
+++ plugins/wpe-headless/headless-poc.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: WPEngine Headless Plugin
- * Plugin URI: https://wpengine.com
+ * Plugin Name: WPEngine Headless
+ * Plugin URI: https://wpengine.com/
  * Description: Plugin for working with headless WordPress.
  * Author: WPEngine
- * Author URI: https://wpengine.com
+ * Author URI: https://wpengine.com/
  * Text Domain: wpe-headless
  * Domain Path: /languages
  * Version: 0.1.1-dev
@@ -23,13 +23,23 @@ require WPE_HEADLESS_DIR . '/includes/settings/functions.php';
 require WPE_HEADLESS_DIR . '/includes/replacement/functions.php';
 require WPE_HEADLESS_DIR . '/includes/updates/functions.php';
 
+require WPE_HEADLESS_DIR . '/includes/class.wpe-headless.php';
+require WPE_HEADLESS_DIR . '/includes/redirect/class.wpe-headless-redirect.php';
+require WPE_HEADLESS_DIR . '/includes/rest/class.wpe-headless-api.php';
+require WPE_HEADLESS_DIR . '/includes/settings/class.wpe-headless-constants.php';
+require WPE_HEADLESS_DIR . '/includes/settings/class.wpe-headless-crypto.php';
+
+require WPE_HEADLESS_DIR . '/includes/menus/callbacks.php';
 require WPE_HEADLESS_DIR . '/includes/replacement/callbacks.php';
 require WPE_HEADLESS_DIR . '/includes/replacement/graphql-callbacks.php';
 require WPE_HEADLESS_DIR . '/includes/settings/callbacks.php';
 require WPE_HEADLESS_DIR . '/includes/updates/callbacks.php';
 
-require WPE_HEADLESS_DIR . '/includes/menus/callbacks.php';
-
 if ( wpe_headless_is_events_enabled() ) {
 	require WPE_HEADLESS_DIR . '/includes/events/callbacks.php';
 }
+
+register_activation_hook(__FILE__, array('WPE_Headless', 'activate'));
+register_deactivation_hook(__FILE__, array('WPE_Headless', 'deactivate'));
+
+WPE_Headless::init();
