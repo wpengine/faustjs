1d088cbb08059ea9bd959f0bcbf01f267d023766
Nick Cernis <nick@cern.is>
2020-11-30 13:51:48 +0100

    build: add webpack config

---
+++ packages/headless-components/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+module.exports = {
+  entry: './src/index.js',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '@wpengine/headless-components',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  plugins: [new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
