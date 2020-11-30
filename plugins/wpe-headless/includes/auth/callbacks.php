d7752b71ceb8027cbdc099074f096dfaa81e3768
Ryan Meier <ryan.meier@wpengine.com>
2020-11-30 14:32:55 -0600

    Switched to wp_safe_redirect() to handle redirects

--- plugins/wpe-headless/includes/auth/callbacks.php
+++ plugins/wpe-headless/includes/auth/callbacks.php
@@ -18,37 +18,55 @@ add_action( 'parse_request', 'wpe_headless_handle_generate_endpoint' );
  * @return void
  */
 function wpe_headless_handle_generate_endpoint() {
-	if ( ! preg_match( '/^\/generate/', $_SERVER['REQUEST_URI'] ) ) {
+	if ( ! preg_match( '/^\/generate/', $_SERVER['REQUEST_URI'] ) ) { // phpcs:ignore WordPress.Security
 		return;
 	}
 
-	if ( empty( $_GET['redirect_uri'] ) ) {
+	if ( empty( $_GET['redirect_uri'] ) ) { // phpcs:ignore WordPress.Security
 		return;
 	}
 
-	$redirect_uri = $_GET['redirect_uri'];
+	$redirect_uri = wp_unslash( $_GET['redirect_uri'] ); // phpcs:ignore WordPress.Security
 
 	if ( ! is_user_logged_in() ) {
-		wp_redirect(
-			wp_login_url( '/generate/?redirect_uri=' . urlencode( $redirect_uri ) )
+		wp_safe_redirect(
+			wp_login_url( '/generate/?redirect_uri=' . rawurlencode( $redirect_uri ) )
 		);
 
 		exit;
 	}
 
-	if ( ! wpe_headless_can_redirect( $redirect_uri ) ) {
-		return;
-	}
-
 	$code = wpe_headless_generate_user_code( wp_get_current_user() );
 
-	if ( parse_url( $redirect_uri, PHP_URL_QUERY ) ) {
-	    $redirect_uri .= "&code={$code}";
+	if ( wp_parse_url( $redirect_uri, PHP_URL_QUERY ) ) {
+		$redirect_uri .= "&code={$code}";
 	} else {
-	    $redirect_uri .= "?code={$code}";
+		$redirect_uri .= "?code={$code}";
 	}
 
-	wp_redirect( $redirect_uri );
+	wp_safe_redirect( $redirect_uri );
 
 	exit;
+}
+
+add_filter( 'allowed_redirect_hosts', 'wpe_headless_allowed_redirect_hosts', 10, 2 );
+/**
+ * Callback for WordPress 'allowed_redirect_hosts' filter.
+ *
+ * Add frontend_uri host and development domains to allowed redirects.
+ *
+ * @param string[] $hosts An array of allowed host names.
+ * @param string   $host  The host name of the redirect destination; empty string if not set.
+ *
+ * @return string[] An array of allowed host names.
+ */
+function wpe_headless_allowed_redirect_hosts( $hosts, $host ) {
+	$hosts         = wp_parse_args( $hosts, array( 'localhost', '0.0.0.0' ) );
+	$frontend_host = wp_parse_url( wpe_headless_get_setting( 'frontend_uri' ), PHP_URL_HOST );
+
+	if ( $frontend_host ) {
+		$hosts[] = $frontend_host;
+	}
+
+	return $hosts;
 }
