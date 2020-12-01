c2ffae2b32daf6ee3bd4308887406e8335451ae5
Clay Griffiths <clay@claygriffiths.com>
2020-12-01 16:20:21 -0600

    chore: remove existing @wpengine/headless code
    Previewing is slated to be rewritten with Next's preview mode so it makes the most sense to sideline this code for now

--- packages/headless/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { gql, useQuery } from '@apollo/client';
-export { ApiProvider } from './Provider';
-export { useClient } from './client';
-export * from './middleware';
-export * from './hooks';
