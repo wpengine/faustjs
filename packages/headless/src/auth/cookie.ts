91631aa73cad1a5169d7086c21423f20f53f5b57
William Johnston <will@wwj.dev>
2020-12-07 08:01:14 -0800

    always trimming WP url

--- packages/headless/src/auth/cookie.ts
+++ packages/headless/src/auth/cookie.ts
@@ -1,10 +1,10 @@
 import { ServerResponse } from 'http';
 import Cookies from 'universal-cookie';
-import { base64Decode, base64Encode } from '../utils';
+import { base64Decode, base64Encode, trimTrailingSlash } from '../utils';
 
 let cookies = new Cookies();
 const WP_URL =
-  process.env.NEXT_PUBLIC_WORDPRESS_URL || process.env.WORDPRESS_URL;
+  trimTrailingSlash(process.env.NEXT_PUBLIC_WORDPRESS_URL || process.env.WORDPRESS_URL);
 const TOKEN_KEY = `${WP_URL as string}-at`;
 
 export function initializeServerCookie(cookie: string | undefined) {
