09600f0a3497378771ab9d9d128976064205f2e3
William Johnston <will@wwj.dev>
2020-12-14 11:55:19 -0800

    chore: documenting package functions and interfaces

--- packages/headless/src/config/config.ts
+++ packages/headless/src/config/config.ts
@@ -3,6 +3,13 @@ import { normalizeConfig } from '../utils';
 
 let wpeConfig: HeadlessConfig = {};
 
+/**
+ * A setter/getter for the HeadlessConfig
+ *
+ * @export
+ * @param {HeadlessConfig} [config]
+ * @returns {HeadlessConfig}
+ */
 export function headlessConfig(config?: HeadlessConfig): HeadlessConfig {
   if (config) {
     wpeConfig = normalizeConfig(config);
