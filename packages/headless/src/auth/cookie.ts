644b743b71324e33cc9fa885a65df8c2daea89a6
Clay Griffiths <clay@claygriffiths.com>
2020-12-27 08:03:07 -0600

    refactor: update initializeNextServerSideProps to use Next.js preview mode

--- packages/headless/src/auth/cookie.ts
+++ packages/headless/src/auth/cookie.ts
@@ -2,15 +2,11 @@ import { ServerResponse } from 'http';
 import Cookies from 'universal-cookie';
 import { base64Decode, base64Encode, trimTrailingSlash } from '../utils';
 
-let cookies = new Cookies();
+const cookies = new Cookies();
 const WP_URL = trimTrailingSlash(
   process.env.NEXT_PUBLIC_WORDPRESS_URL || process.env.WORDPRESS_URL,
 );
 const TOKEN_KEY = `${WP_URL as string}-at`;
-
-export function initializeServerCookie(cookie: string | undefined) {
-  cookies = new Cookies(cookie);
-}
 
 /* eslint-disable consistent-return */
 /**
