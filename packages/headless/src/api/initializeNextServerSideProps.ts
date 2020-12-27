4757b5d1804330be30c2004330a18f0705a54487
Clay Griffiths <clay@claygriffiths.com>
2020-12-27 08:03:07 -0600

    refactor: update initializeNextServerSideProps to use Next.js preview mode

--- packages/headless/src/api/initializeNextServerSideProps.ts
+++ packages/headless/src/api/initializeNextServerSideProps.ts
@@ -1,37 +1,39 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { getUriInfo, getPosts, getContentNode } from './services';
 import { initializeApollo, addApolloState } from '../provider';
-import { initializeServerCookie, storeAccessToken } from '../auth';
-import { ensureAuthorization } from '../auth/authorize';
 import { headlessConfig } from '../config';
 import { ContentNodeIdType, UriInfo } from '../types';
 import { resolvePrefixedUrlPath } from '../utils';
+import getCurrentPath from '../utils/getCurrentPath';
+import { ensureAuthorization } from '../auth';
+import {isPreview, isPreviewPath} from "../utils/preview";
 
 /**
  * Must be called from getServerSideProps within a Next app in order to support SSR. It will
  * initialized cookies and prefetch/cache the page content and bundle it with the page for
  * rehydration on the frontend.
  *
- * @async
- * @export
  * @param {GetServerSidePropsContext} context The Next SSR context
- * @returns {Promise<GetServerSidePropsResult<unknown>>}
  */
 export async function initializeNextServerSideProps(
   context: GetServerSidePropsContext,
 ): Promise<GetServerSidePropsResult<unknown>> {
   const apolloClient = initializeApollo();
 
-  initializeServerCookie(context.req.headers.cookie);
-
   const wpeConfig = headlessConfig();
+
   const currentUrlPath = resolvePrefixedUrlPath(
-    context.resolvedUrl ?? '',
+    getCurrentPath(context),
     wpeConfig.uriPrefix,
   );
-  const pageInfo = (await getUriInfo(apolloClient, currentUrlPath)) as UriInfo;
 
-  if (pageInfo.isPreview) {
+  const pageInfo = (await getUriInfo(
+    apolloClient,
+    currentUrlPath,
+    isPreview(context),
+  )) as UriInfo;
+
+  if (isPreview(context)) {
     const host = context.req.headers.host as string;
     const protocol = /localhost/.test(host) ? 'http:' : 'https:';
     const response = ensureAuthorization(
@@ -40,7 +42,7 @@ export async function initializeNextServerSideProps(
       }`,
     );
 
-    if (typeof response !== 'string' && typeof response.redirect === 'string') {
+    if (typeof response !== 'string' && response?.redirect) {
       return {
         redirect: {
           permanent: false,
@@ -48,26 +50,25 @@ export async function initializeNextServerSideProps(
         },
       };
     }
+  } else if (isPreviewPath(context)) {
+    return {
+      notFound: true,
+      props: {},
+    };
   }
 
-  try {
-    if (pageInfo.isPostsPage) {
-      await getPosts(apolloClient);
-    } else {
-      await getContentNode(
-        apolloClient,
-        pageInfo.uriPath,
-        ContentNodeIdType.URI,
-        pageInfo.isPreview,
-      );
-    }
-  } catch (e) {
-    storeAccessToken(undefined, context.res);
+  if (pageInfo.isPostsPage) {
+    await getPosts(apolloClient);
+  } else {
+    await getContentNode(
+      apolloClient,
+      pageInfo.uriPath,
+      ContentNodeIdType.URI,
+      isPreview(context),
+    );
   }
 
   return addApolloState(apolloClient, {
     props: { preview: context.preview ?? false },
-  }) as {
-    props: unknown;
-  };
+  });
 }
