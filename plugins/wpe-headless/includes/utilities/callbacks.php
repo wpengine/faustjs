beea62ed988e67cf57e98629848641c5355b2dbc
Ryan Meier <ryan.meier@wpengine.com>
2021-01-04 13:28:29 -0600

    Added default settings when plugin is activated

--- plugins/wpe-headless/includes/utilities/callbacks.php
+++ plugins/wpe-headless/includes/utilities/callbacks.php
@@ -13,7 +13,9 @@ register_activation_hook( WPE_HEADLESS_FILE, 'wpe_headless_handle_activation' );
 /**
  * Callback for WordPress register_activation_hook() function.
  *
- * Set the secret key and flush rewrite rules.
+ * 1. Set default settings if no settings exist.
+ * 2. Set secret_key if does not exist.
+ * 3. Flush rewrite rules.
  *
  * @todo is flush_rewrite_rules() needed?
  *
@@ -23,6 +25,13 @@ register_activation_hook( WPE_HEADLESS_FILE, 'wpe_headless_handle_activation' );
  */
 function wpe_headless_handle_activation() {
 	$secret_key = wpe_headless_get_secret_key();
+	$settings   = wpe_headless_get_settings();
+
+	if ( empty( $settings ) ) {
+		wpe_headless_update_setting( 'disable_theme', '1' );
+		wpe_headless_update_setting( 'enable_rewrites', '1' );
+		wpe_headless_update_setting( 'enable_redirects', '1' );
+	}
 
 	if ( ! $secret_key ) {
 		wpe_headless_update_setting( 'secret_key', wp_generate_uuid4() );
