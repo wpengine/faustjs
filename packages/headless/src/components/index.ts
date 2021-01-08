3f155ef179012ccbba3a6c21c5cb7c12305a08a8
Clay Griffiths <clay@claygriffiths.com>
2021-01-08 10:56:00 -0600

    feat: add <WPHead /> component
    it currently handles just the <title>

--- packages/headless/src/components/index.ts
+++ packages/headless/src/components/index.ts
@@ -1,5 +1,6 @@
 import Menu from './menu/Menu';
 import MenuItem from './menu/MenuItemInterface';
 import TemplateLoader from './TemplateLoader';
+import WPHead from './WPHead';
 
-export { Menu, MenuItem, TemplateLoader };
+export { Menu, MenuItem, TemplateLoader, WPHead };
