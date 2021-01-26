26800a7c268245048f43c15f2f93dfb7a1f8289a
Mark Kelnar <markkelnar@gmail.com>
2021-01-26 13:20:49 -0600

    feat: Add conent image srcset filter

--- plugins/wpe-headless/tests/integration/replacement/test-replacement-callbacks.php
+++ plugins/wpe-headless/tests/integration/replacement/test-replacement-callbacks.php
@@ -58,15 +58,39 @@ class ReplacementCallbacksTestCases extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests wpe_headless_content_media_replacement() replaces the frontend_uri value when content replacement is enabled.
+	 * Tests wpe_headless_content_media_src_replacement() replaces the frontend_uri value when content replacement is enabled.
 	 */
-	public function test_wpe_headless_content_media_replacement_filters_content_when_content_replacement_enabled() {
+	public function test_wpe_headless_content_media_src_replacement_filters_content_when_content_replacement_enabled() {
 		wpe_headless_update_setting( 'frontend_uri', 'http://foo.co' );
 		add_filter( 'wpe_headless_domain_replacement_enabled', '__return_true' );
 		# Do not replace partial domain match.
 		$this->assertSame( '<img src="http://foo.com/image.png">', wpe_headless_content_media_replacement( '<img src="http://foo.com/image.png">' ) );
 		# Do replace exact domain match
 		$this->assertSame( '<img src="http://example.org/image.png">', wpe_headless_content_media_replacement( '<img src="http://foo.co/image.png">' ) );
+		$this->assertSame( '<img src="http://example.org/image.png">', wpe_headless_content_media_replacement( '<img src="/image.png">' ) );
+		wpe_headless_update_setting( 'frontend_uri', null );
+		remove_filter( 'wpe_headless_domain_replacement_enabled', '__return_true' );
+	}
+
+	/**
+	 * Tests wpe_headless_content_media_srcset_replacement() replaces the frontend_uri value when content replacement is enabled.
+	 */
+	public function test_wpe_headless_content_media_srcset_replacement_filters_content_when_content_replacement_enabled() {
+		wpe_headless_update_setting( 'frontend_uri', 'http://foo.co' );
+		add_filter( 'wpe_headless_domain_replacement_enabled', '__return_true' );
+		# Do not replace partial domain match.
+		# Do replace exact domain match
+		$sources = array (
+			100 => array('url' => 'http://foo.com/image100.png'),
+			300 => array('url' => '/wp-content/uploads/image300.jpg'),
+			400 => array('url' => 'http://foo.co/image400.png'),
+		);
+		$expected = array (
+			100 => array('url' => 'http://foo.com/image100.png'),
+			300 => array('url' => 'http://example.org/wp-content/uploads/image300.jpg'),
+			400 => array('url' => 'http://example.org/image400.png'),
+		);
+		$this->assertSame( $expected, wpe_headless_content_media_srcset_replacement( $sources ) );
 		wpe_headless_update_setting( 'frontend_uri', null );
 		remove_filter( 'wpe_headless_domain_replacement_enabled', '__return_true' );
 	}
