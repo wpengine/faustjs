d5ff8582a063483d5dbc39b43fe7f957db5cdb01
Clay Griffiths <clay@claygriffiths.com>
2021-01-26 15:10:47 -0600

    fix: do not export resolveTemplate in /utils to reduce chance of Webpack errors
    This should prevent errors if the 'theme' directory is not present in a non-Next.js project

--- packages/headless/src/utils/index.ts
+++ packages/headless/src/utils/index.ts
@@ -2,4 +2,5 @@ export * from './assert';
 export * from './convert';
 export { default as getCurrentPath } from './getCurrentPath';
 export * from './preview';
-export * from './resolveTemplate';
+/* resolveTemplate is intentionally excluded here to prevent Webpack errors for setups that may not
+  have a "theme" directory */
