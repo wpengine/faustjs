af85a5d0b5c439f49ff91be8de740aa4bb7d914e
Nick Cernis <nick@cern.is>
2021-02-02 11:10:13 +0100

    refactor: Use Google Fonts instead of local fonts
    Addresses feedback from @trevanhetzel at
    https://github.com/wpengine/headless-framework/pull/109#discussion_r568136700.

--- examples/preview/components/Header.tsx
+++ examples/preview/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { WPHead } from '@wpengine/headless';
 import styles from 'scss/components/Header.module.scss';
 import Link from 'next/link';
+import Head from 'next/head';
 
 interface Props {
   title?: string;
@@ -22,6 +23,26 @@ function Header({ title = 'Headless by WP Engine', description }: Props) {
 
   return (
     <>
+      <Head>
+        <title>{/* Title is required here but replaced by WPHead. */}</title>
+        {/* Add extra elements to <head> here. */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?display=swap&amp;family=Public%20Sans%3Aital%2Cwght%400%2C100..900%3B1%2C100..900&amp;subset=latin%2Clatin-ext"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?display=swap&amp;family=Public%20Sans%3Aital%2Cwght%400%2C100..900%3B1%2C100..900&amp;subset=latin%2Clatin-ext"
+          type="text/css"
+          media="all"
+        />
+      </Head>
       <WPHead />
       <header>
         <div className={styles.wrap}>
