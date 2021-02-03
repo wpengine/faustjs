e0c289dec9df68f657c0b9cefb6c88fb62d26968
Nick Cernis <nick@cern.is>
2021-02-03 09:59:46 +0100

    fix: Remove front page template loader from index
    Clay's fix in https://github.com/wpengine/headless-framework/pull/109/commits/9e996a96be95d042a48df69063d3e2aebb84aebf
    ensures the front-page template loads for “latest
    posts” homepages now, so front-page.tsx
    will load automatically as expected. 🎉

--- examples/preview/theme/index.tsx
+++ examples/preview/theme/index.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import {
-  useGeneralSettings,
-  usePosts,
-  useNextUriInfo,
-} from '@wpengine/headless';
+import { useGeneralSettings, usePosts } from '@wpengine/headless';
 import { Footer, Header, Posts } from '../components';
-import FrontPage from './front-page';
 
 export default function Index(): JSX.Element {
   const posts = usePosts();
   const settings = useGeneralSettings();
-  const uri = useNextUriInfo();
-
-  // Loads front-page.tsx if Settings → Reading uses “Your latest posts”.
-  if (uri?.uriPath === '/') {
-    return <FrontPage />;
-  }
 
   return (
     <>
