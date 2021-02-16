d93ab3337651ce25e1295a2ae43dc8d84968cb5c
Nick Cernis <nick@cern.is>
2021-02-16 19:15:50 +0100

    fix: Fetch category slug from path
    Assumes a category prefix of 'category'.
    
    Thanks to @mindctrl.

--- examples/getting-started/wp-templates/category.tsx
+++ examples/getting-started/wp-templates/category.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { useGeneralSettings, usePosts } from '@wpengine/headless/react';
 import { getApolloClient, getPosts } from '@wpengine/headless';
+import { useRouter } from 'next/router';
 import { GetStaticPropsContext } from 'next';
 import { Footer, Header, Posts } from '../components';
 
 export default function Category(): JSX.Element {
+  const router = useRouter();
+  const urlParts = router.asPath.split('/').filter(Boolean);
+  const category = urlParts[1];
+  // const pageNo = urlParts[3];
   const posts = usePosts({
     variables: {
-      where: { categoryName: 'uncategorized' }, // TODO: Extract the category slug from the URL.
+      where: { categoryName: category },
       // TODO: add pagination vars.
     },
   });
-
   const settings = useGeneralSettings();
 
   return (
@@ -29,10 +33,16 @@ export default function Category(): JSX.Element {
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
+  let urlParts: Array<string> = [];
+  if (Array.isArray(context?.params?.page) && context?.params?.page) {
+    urlParts = context?.params?.page;
+  }
+  const category = urlParts[1];
+  // const pageNo = urlParts[3];
   const client = getApolloClient(context);
   await getPosts(client, {
     variables: {
-      where: { categoryName: 'uncategorized' }, // TODO: extract the category slug from the URL.
+      where: { categoryName: category },
       // TODO: add pagination vars.
     },
   });
