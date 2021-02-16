c9106a10de22416421518ae3b34dfeca5a0fb7ea
Nick Cernis <nick@cern.is>
2021-02-16 18:00:52 +0100

    refactor: Remove PostsArchive component
    Just use Posts instead.

--- examples/getting-started/components/index.ts
+++ examples/getting-started/components/index.ts
@@ -3,6 +3,5 @@ import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import Posts from './Posts';
-import PostsArchive from './PostsArchive';
 
-export { CTA, Footer, Header, Hero, Posts, PostsArchive };
+export { CTA, Footer, Header, Hero, Posts };
