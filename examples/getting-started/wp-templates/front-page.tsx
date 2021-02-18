72747ad947515dca6e2ea4c85edf408afb99d1c9
Nick Cernis <nick@cern.is>
2021-02-18 11:44:10 +0100

    fix (BH-897): Fetch six posts for the front page template (#147)
    * fix: Fetch six posts for the front page template
    
    Fixes #144 so the front page template displays
    posts even if Settings → Reading is using a
    static page instead of “latest posts”.
    
    * docs: remove where clause to get all posts

--- examples/getting-started/wp-templates/front-page.tsx
+++ examples/getting-started/wp-templates/front-page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { usePosts, useGeneralSettings } from '@wpengine/headless/react';
+import { GetStaticPropsContext } from 'next';
+import { getApolloClient, getPosts } from '@wpengine/headless';
 import { CTA, Header, Footer, Hero, Posts } from '../components';
 import styles from '../scss/wp-templates/front-page.module.scss';
 
+/**
+ * Example of post variables to query the first six posts in a named category.
+ * @see https://github.com/wpengine/headless-framework/tree/canary/docs/queries
+ */
+const firstSixInCategory = {
+  variables: {
+    first: 6,
+    where: { categoryName: 'uncategorized' }, // Omit this to get posts from all categories.
+  },
+};
+
 export default function FrontPage(props: any): JSX.Element {
-  const posts = usePosts();
+  const posts = usePosts(firstSixInCategory);
   const settings = useGeneralSettings();
 
   return (
@@ -131,4 +144,17 @@ export default function FrontPage(props: any): JSX.Element {
       <Footer copyrightHolder={settings?.title} />
     </>
   );
+}
+
+/**
+ * Get additional data from WordPress that is specific to this template.
+ *
+ * Here we retrieve the latest six WordPress posts in a named category to
+ * display at the bottom of the front page.
+ *
+ * @see https://github.com/wpengine/headless-framework/tree/canary/docs/queries
+ */
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const client = getApolloClient(context);
+  await getPosts(client, firstSixInCategory);
 }
