29931e4fb2077838929272a12c399f8327968245
Nick Cernis <nick@cern.is>
2021-02-22 14:17:55 +0100

    feat: Add pagination to getting-started template
    To demonstrate a paginated archive. Will show
    next and previous page links on category
    archives such as category/uncategorized.

--- examples/getting-started/wp-templates/category.tsx
+++ examples/getting-started/wp-templates/category.tsx
@@ -1,42 +1,90 @@
 import React from 'react';
 import { useGeneralSettings, usePosts } from '@wpengine/headless/react';
+import { Pagination } from '@wpengine/headless/next';
 import { getApolloClient, getPosts } from '@wpengine/headless';
 import { useRouter } from 'next/router';
 import { GetStaticPropsContext } from 'next';
 import { Footer, Header, Posts } from '../components';
 
+const POSTS_PER_PAGE = 10;
+
+/**
+ * Parts of an optionally paginated category URL path such as
+ * /category/uncategorized/[before|after]/[abc123].
+ */
+interface CategoryUrlParts {
+  basePath: string; // '/category/uncategorized'
+  category: string; // 'uncategorized'
+  keyword?: string; // 'before'
+  id?: string; // 'abc123'
+}
+
+/**
+ * Converts a string or array into category URL parts assuming the
+ * form /category/uncategorized/[before|after]/[abc123].
+ */
+const getCategoryUrlParts = (path: string | string[]): CategoryUrlParts => {
+  const parts = Array.isArray(path) ? path : path.split('/').filter(Boolean);
+  return {
+    basePath: parts.slice(0, 2).join('/'),
+    category: parts[1],
+    keyword: parts[2],
+    id: parts[3],
+  };
+};
+
+/**
+ * Determines WPGraphQL query options from URL info.
+ *
+ * @see https://www.wpgraphql.com/2020/03/26/forward-and-backward-pagination-with-wpgraphql/.
+ */
+const getQueryOptions = (url: CategoryUrlParts, postsPerPage: number) => {
+  return {
+    variables: {
+      first: url.keyword === 'after' || !url.keyword ? postsPerPage : undefined,
+      last: url.keyword === 'before' ? postsPerPage : undefined,
+      after: url.keyword === 'after' ? url.id : undefined,
+      before: url.keyword === 'before' ? url.id : undefined,
+      where: {
+        categoryName: url.category,
+      },
+    },
+  };
+};
+
+/**
+ * The component to display on category archives.
+ */
 export default function Category(): JSX.Element {
   const router = useRouter();
-  const urlParts = router.asPath.split('/').filter(Boolean);
-  const category = urlParts[1];
-  const posts = usePosts({
-    variables: {
-      where: { categoryName: category },
-    },
-  });
   const settings = useGeneralSettings();
+  const url = getCategoryUrlParts(router.asPath);
+  const options = getQueryOptions(url, POSTS_PER_PAGE);
+  const posts = usePosts(options);
 
   return (
     <>
       <Header title={settings?.title} description={settings?.description} />
       <main className="content content-index">
         <Posts posts={posts?.nodes} />
+        {posts?.pageInfo && (
+          <Pagination baseURL={url.basePath} pageInfo={posts.pageInfo} />
+        )}
       </main>
       <Footer copyrightHolder={settings?.title} />
     </>
   );
 }
 
+/**
+ * Fetch posts from WordPress. The query options in `getPosts()` here must match
+ * those from `usePosts()` in the component above.
+ *
+ * @see https://github.com/wpengine/headless-framework/tree/canary/docs/queries
+ */
 export async function getStaticProps(context: GetStaticPropsContext) {
-  let urlParts: Array<string> = [];
-  if (Array.isArray(context?.params?.page) && context?.params?.page) {
-    urlParts = context?.params?.page;
-  }
-  const category = urlParts[1];
+  const url = getCategoryUrlParts(context?.params?.page || []);
   const client = getApolloClient(context);
-  await getPosts(client, {
-    variables: {
-      where: { categoryName: category },
-    },
-  });
+  const options = getQueryOptions(url, POSTS_PER_PAGE);
+  await getPosts(client, options);
 }
