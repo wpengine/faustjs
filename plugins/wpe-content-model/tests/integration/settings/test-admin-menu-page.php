cc971d8eca7351a351a3a94f2877788ad2cb80bd
John Parris <john.parris@wpengine.com>
2021-02-25 09:29:41 -0500

    chore: Remove content model plugin and Circle CI config

--- plugins/wpe-content-model/tests/integration/settings/test-admin-menu-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Class AdminMenuPageTest
- */
-class AdminMenuPageTest extends WP_UnitTestCase {
-	public function test_admin_menu_hook_has_action_added(): void {
-		self::assertSame( 10, has_action( 'admin_menu', 'WPE\ContentModel\Settings\register_admin_menu_page' ) );
-	}
-}
