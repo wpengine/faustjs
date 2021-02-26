ca79517187edd79dd614252ac58f3818ac780729
William Johnston <will@wwj.dev>
2021-02-26 14:31:55 -0800

    refactor: JS is old-browser friendly

--- plugins/wpe-headless/includes/replacement/previewlinks.js
+++ plugins/wpe-headless/includes/replacement/previewlinks.js
@@ -1,19 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-  wp.domReady(() => {
-    const intervalId = setInterval(() => {
+window.addEventListener('DOMContentLoaded', function () {
+  wp.domReady(function () {
+    const intervalId = setInterval(function () {
       const previewButton = document.querySelectorAll(
         'button[class~="block-editor-post-preview__button-toggle"]',
       );
 
-      if (!previewButton) return;
+      if (!previewButton) {
+        return;
+      }
+
       clearInterval(intervalId);
-      previewButton[0].addEventListener('click', () => {
-        setTimeout(() => {
+      previewButton[0].addEventListener('click', function () {
+        setTimeout(function () {
           const link = document.querySelector(
             'a[class~="edit-post-header-preview__button-external"]',
           );
           if (!!link && !/\/preview(\/\w|\?)/.test(link.href)) {
-            link.href = `${link.protocol}//${link.host}${link.pathname.replace(/\/$/, '')}/preview${link.search}`;
+            link.href =
+              link.protocol +
+              '//' +
+              link.host +
+              link.pathname.replace(/\/$/, '') +
+              '/preview' +
+              link.search;
           }
         }, 100);
       });
