488695a5c66c360d2be86ca9af8a0fe6bd5ccc16
Blake Wilson <blake.wilson@wpengine.com>
2021-05-17 12:26:34 -0500

    fix: include next definition reference

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="./types/next.d.ts" />
 /// <reference path="./types/wpgraphql.d.ts" />
 
 import { authorizeHandler } from '@wpengine/headless-core';
