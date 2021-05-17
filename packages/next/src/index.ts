d41fbb368293a9911d13f4a122b69e6a017f5e0b
Blake Wilson <blake.wilson@wpengine.com>
2021-05-17 09:19:02 -0500

    fix: export HeadlessProvider for headless-next

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,4 +1,8 @@
+import { HeadlessProvider } from '@wpengine/headless-react';
+
 export * from './getProps';
 export * from './getStaticPaths';
 export * from './hooks';
 export * from './WPHead';
+
+export { HeadlessProvider };
