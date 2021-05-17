19034d80655a601df3e56ac94ec929e87b823557
Blake Wilson <blake.wilson@wpengine.com>
2021-05-17 12:15:19 -0500

    fix: include reference to wpgraphql definition

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="./types/wpgraphql.d.ts" />
+
 import { authorizeHandler } from '@wpengine/headless-core';
 import { HeadlessProvider } from '@wpengine/headless-react';
 
