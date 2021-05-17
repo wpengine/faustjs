2069c061bc25389ff61265b8b821dc5ee6366483
Blake Wilson <blake.wilson@wpengine.com>
2021-05-17 12:43:12 -0500

    refactor: remove WPHead

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -8,6 +8,5 @@ import { HeadlessProvider } from '@wpengine/headless-react';
 export * from './getProps';
 export * from './getStaticPaths';
 export * from './hooks';
-export * from './WPHead';
 
 export { authorizeHandler, HeadlessProvider };
