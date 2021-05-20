340533bba932c8b190c612de422fe7af2e08b49d
Blake Wilson <blake.wilson@wpengine.com>
2021-05-20 14:49:20 -0500

    fix: revert next package changes

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -2,23 +2,11 @@
 /// <reference path="./types/next.d.ts" />
 /// <reference path="./types/wpgraphql.d.ts" />
 
-import {
-  authorizeHandler,
-  headlessConfig,
-  getApolloClient,
-  getPosts,
-} from '@wpengine/headless-core';
-import { HeadlessProvider, useGeneralSettings } from '@wpengine/headless-react';
+import { authorizeHandler } from '@wpengine/headless-core';
+import { HeadlessProvider } from '@wpengine/headless-react';
 
 export * from './getProps';
 export * from './getStaticPaths';
 export * from './hooks';
 
-export {
-  authorizeHandler,
-  HeadlessProvider,
-  headlessConfig,
-  useGeneralSettings,
-  getApolloClient,
-  getPosts,
-};
+export { authorizeHandler, HeadlessProvider };
