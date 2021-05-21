b48bb6122741b8901f10c675a31b6f19b27ceba4
Blake Wilson <blake.wilson@wpengine.com>
2021-05-21 15:52:01 -0500

    refactor: remove re-exports from higher-level headless libs (#229)
    * refactor: remove re-exports from higher-level headless libs
    
    * fix: typescript-eslint and prettier rule conflict
    
    * fix: lint scripts for ci
    
    * fix: remove prettier/@typescript-eslint
    
    * Revert "fix: lint scripts for ci"
    
    This reverts commit f6c86ec120194a87a5efa3ccef80249c953a6c42.
    
    * fix: Use core linting scripts
    
    * fix: ESlint warnings

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -2,11 +2,6 @@
 /// <reference path="./types/next.d.ts" />
 /// <reference path="./types/wpgraphql.d.ts" />
 
-import { authorizeHandler } from '@wpengine/headless-core';
-import { HeadlessProvider } from '@wpengine/headless-react';
-
 export * from './getProps';
 export * from './getStaticPaths';
 export * from './hooks';
-
-export { authorizeHandler, HeadlessProvider };
