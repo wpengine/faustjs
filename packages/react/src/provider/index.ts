c01f2b05c3c3e015df90ee8663c417dbcbe5ec65
Blake Wilson <blake.wilson@wpengine.com>
2021-06-10 15:08:04 -0500

    feat: (#262) add gqless to React package (#265)
    * chore: (#262) removing apollo and adding gqless
    
    * feat: (#262) adding ability to intercept request context and apply headers, adding client-side auth header
    
    * feat: (#262) incrementing versions
    
    * feat: (#262) create client
    
    * refactor: (#262) remove apollo
    
    * refactor: (#262) headless provider
    
    * feat (#262): create hooks using gqless
    
    * feat: (#262) provide client creation options ability
    
    Co-authored-by: William Johnston <will@wwj.dev>

--- packages/react/src/provider/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './apolloClient';
-export * from './HeadlessProvider';
--- packages/react/src/types/request.d.ts
+++
@@ -1,5 +0,0 @@
-interface WithApolloClient {
-  __apollo_client?: import('@apollo/client').ApolloClient<
-    import('@apollo/client').NormalizedCacheObject
-  >;
-}
