9823c3666f21a0517289d31fc584f2395253a2d7
Blake Wilson <blake.wilson@wpengine.com>
2021-06-22 14:15:41 -0500

    chore: clean up docusaurus config

--- internal/website/docusaurus.config.js
+++ internal/website/docusaurus.config.js
@@ -4,30 +4,30 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Headless Framework',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Headless by WP Engine',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wpengine', // Usually your GitHub org/user name.
+  projectName: 'headless-framework', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Headless Framework',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
       items: [
         {
           type: 'doc',
           docId: 'introduction',
-          position: 'left',
+          position: 'right',
           label: 'Docs',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/wpengine/headless-framework',
           label: 'GitHub',
           position: 'right',
         },
@@ -99,7 +99,10 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/wpengine/headless-framework/edit/docs-setup-docusaurus/internal/website/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          routeBasePath: '/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
