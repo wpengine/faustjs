7b07d03587e2b465de312c8744b7d2b7a248f1ad
William Johnston <will@wwj.dev>
2021-07-09 11:01:42 -0700

    doc: (#341) get started button is primary

--- internal/website/src/pages/index.js
+++ internal/website/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--info button--lg"
             to="/next/getting-started">
             Get Started
           </Link>
