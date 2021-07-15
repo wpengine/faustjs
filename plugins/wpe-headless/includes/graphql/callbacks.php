0bc5d728f89c7a6b1aef33710389641aff00e98b
Anthony Burchell <anthony.burchell@wpengine.com>
2021-07-14 20:36:17 -0500

    Return templates object in the template_hierarchy callback (#347)
    * return our templates in the template_hierarchy callback
    
    * properly identifies function as returning array

--- plugins/wpe-headless/includes/graphql/callbacks.php
+++ plugins/wpe-headless/includes/graphql/callbacks.php
@@ -112,11 +112,12 @@ function wpe_headless_templates_resolver( $root, $args, AppContext $context, Res
  *
  * @param string[] $templates Templates being loaded.
  *
- * @return void
+ * @return array
  */
 function wpe_headless_log_template_hierarchy( $templates ) {
 	global $wpe_headless_checked_templates;
 	$wpe_headless_checked_templates = array_merge( $wpe_headless_checked_templates, $templates );
+	return $wpe_headless_checked_templates;
 }
 
 add_action( 'graphql_register_types', 'wpe_headless_register_conditional_tags_field' );
