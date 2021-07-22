c13c2ccbe6ef965cacf5e98c163a26b1362e329d
Blake Wilson <blake.wilson@wpengine.com>
2021-07-22 16:35:25 -0500

    refactor: (#369) env files in examples (#374)
    * refactor: (#369) clean up env samples and remove WORDPRESS_URL in Next
    
    * docs: (#369) reflect env changes in Next example
    
    * fix: (#369) require dotenv for gqless config

--- examples/next/getting-started/src/faust.config.js
+++ examples/next/getting-started/src/faust.config.js
@@ -4,6 +4,6 @@ const { headlessConfig } = require('@faustjs/core');
  * @type {import("@faustjs/core").HeadlessConfig}
  */
 module.exports = headlessConfig({
-  wpUrl: process.env.WORDPRESS_URL || process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL || '',
   apiClientSecret: process.env.WP_HEADLESS_SECRET,
 });
