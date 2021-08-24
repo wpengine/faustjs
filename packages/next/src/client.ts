3e35382f66ca7f7f5b2372f1366819e6d1660975
Blake Wilson <blake.wilson@wpengine.com>
2021-08-24 11:14:50 -0500

    Revert "fix: useLogin typings"
    This reverts commit 7dfec696a2b179a76980378a83e9d1c075964cb0.

--- packages/next/src/client.ts
+++ packages/next/src/client.ts
@@ -109,7 +109,8 @@ export interface NextClient<
           code?: undefined;
         }
       | {
-          code: string;
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          code: any;
           error?: undefined;
         }
       | undefined;
