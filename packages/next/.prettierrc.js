8c0034a7e6f162b02d272aa1ab5b89ac339e57e7
Blake Wilson <blake@blake.id>
2021-08-26 09:56:53 -0500

    make: (#416) format script

--- packages/next/.prettierrc.js
+++ packages/next/.prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    arrowParens: 'always',
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    tabWidth: 2,
-    semi: true,
-    trailingComma: 'all',
-    endOfLine: 'lf',
-    useTabs: false,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  useTabs: false,
 };
