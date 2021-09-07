10c66eabec2b733307db69611badd9a078b5e610
Blake Wilson <blake@blake.id>
2021-09-07 12:32:01 -0500

    fix: linting scripts/packages

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
+    'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:@typescript-eslint/eslint-recommended',
