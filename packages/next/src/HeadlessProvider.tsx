03cd8f4bfd601f24aad806556f1582d6b723ab69
William Johnston <will@wwj.dev>
2021-09-07 12:08:43 -0700

    fix: calling useContext from useClient always

--- packages/next/src/HeadlessProvider.tsx
+++ packages/next/src/HeadlessProvider.tsx
@@ -16,16 +16,12 @@ export function HeadlessProvider<Props = Record<string, unknown>>({
   pageProps: PageProps<Props>['props'];
   client: ReturnType<typeof getClient>;
 }>): JSX.Element {
-  const rootClient = client.useClient();
-
-  if (rootClient === client) {
-    client.setAsRoot();
-  }
+  client.setAsRoot();
 
   const {
     useHydrateCache,
     auth: { useHydrateCache: useAuthHydrateCache },
-  } = rootClient;
+  } = client;
   const cacheSnapshot = pageProps[CLIENT_CACHE_PROP];
   const authSnapshot = pageProps[AUTH_CLIENT_CACHE_PROP];
 
@@ -40,7 +36,7 @@ export function HeadlessProvider<Props = Record<string, unknown>>({
   return (
     <HeadlessContext.Provider
       value={{
-        client: rootClient,
+        client,
       }}>
       {children}
     </HeadlessContext.Provider>
