c7436ef2114f565e5c4dc56a7d38242939950084
Blake Wilson <blake.wilson@wpengine.com>
2021-09-14 12:21:46 -0500

    Use site-dev as the edit url for docs

--- internal/website/docusaurus.config.js
+++ internal/website/docusaurus.config.js
@@ -103,7 +103,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/wpengine/faustjs/edit/canary/internal/website/',
+            'https://github.com/wpengine/faustjs/edit/site-dev/internal/website/',
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           routeBasePath: '/docs',
