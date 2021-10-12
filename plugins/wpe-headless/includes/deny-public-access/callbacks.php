5a11621b087cfc49d81dcd5c1a77d5371ee30c00
Andrew Matthews <andrew.matthews@wpengine.com>
2021-10-12 18:04:46 -0400

    refactor: simplify public route redirection by using template_redirect

--- plugins/wpe-headless/includes/deny-public-access/callbacks.php
+++ plugins/wpe-headless/includes/deny-public-access/callbacks.php
@@ -9,31 +9,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'parse_request', 'wpe_headless_deny_public_access', 99 );
+add_action( 'template_redirect', 'wpe_headless_deny_public_access', 99 );
 /**
  * Redirects non-API requests for public URLs to the specified front-end URL.
  *
- * @param object $query The current query.
- *
  * @return void
  */
-function wpe_headless_deny_public_access( $query ) {
-	if ( ! wpe_headless_is_redirects_enabled() ) {
+function wpe_headless_deny_public_access() {
+	if ( ! wpe_headless_is_redirects_enabled() || is_customize_preview() ) {
 		return;
 	}
 
 	$frontend_uri = wpe_headless_get_setting( 'frontend_uri' );
 
-	if (
-		defined( 'DOING_CRON' ) ||
-		defined( 'REST_REQUEST' ) ||
-		is_admin() ||
-		is_customize_preview() ||
-		( function_exists( 'is_graphql_http_request' ) && is_graphql_http_request() ) || // From https://wordpress.org/plugins/wp-graphql/.
-		! empty( $query->query_vars['rest_oauth1'] ) || // From https://oauth1.wp-api.org/.
-		! property_exists( $query, 'request' ) ||
-		! $frontend_uri
-	) {
+	if ( ! $frontend_uri ) {
 		return;
 	}
 
