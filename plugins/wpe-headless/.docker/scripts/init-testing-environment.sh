e72ea10b3e4e3389d0b8661bdf9d3776a33dbf23
Blake Wilson <blake.wilson@wpengine.com>
2021-10-19 12:37:32 -0500

    Run unit tests via docker/github actions

---
+++ plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /var/www/html/
+
+#1. check if wordpress is already installed/configured
+if (sudo -u www-data -- wp core is-installed)
+then
+
+	#2. check if the database is ready
+	if ! (sudo -u www-data -- wp db check)
+	then
+		# wait a moment for the database container
+		sleep 1
+		exit 1;
+	fi
+
+	#3. init the testing instance
+	cd wp-content/plugins/$WP_PLUGIN_FOLDER && sudo -u www-data -- bash -c "./bin/install-wp-tests.sh $WP_TESTS_DB_NAME $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_DB_HOST latest"	
+fi
+
+#4. back to the root WP folder
+cd /var/www/html/
