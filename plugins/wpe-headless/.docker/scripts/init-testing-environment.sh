594a50c6e9e1149bd7c5bcc8a0f16a7b7ad9bbca
Blake Wilson <blake.wilson@wpengine.com>
2021-10-19 14:36:25 -0500

    Composer install

--- plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
+++ plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
@@ -8,5 +8,7 @@ cd wp-content/plugins/$WP_PLUGIN_FOLDER
 
 bash -c "./bin/install-wp-tests.sh $WP_TESTS_DB_NAME $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_DB_HOST latest" 
 
+composer install
+
 #4. back to the root WP folder
 cd /var/www/html/
