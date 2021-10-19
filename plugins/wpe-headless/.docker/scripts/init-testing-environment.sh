32bb32430e5d2a70a239d521316623802c7a56d1
Blake Wilson <blake.wilson@wpengine.com>
2021-10-19 15:13:48 -0500

    Clean up test action

--- plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
+++ plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd /var/www/html/
+cd /var/www/html/wp-content/plugins/$WP_PLUGIN_FOLDER
 
-ls
-
-cd wp-content/plugins/$WP_PLUGIN_FOLDER
-
+# Setup WordPress test core files and database
 bash -c "./bin/install-wp-tests.sh $WP_TESTS_DB_NAME $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_DB_HOST latest" 
 
+# Install composer deps
 composer install
 
-#4. back to the root WP folder
+# Back to the root WP folder
 cd /var/www/html/
