5ed87fb9e95a2277db0274852bc183c696e85736
Blake Wilson <blake.wilson@wpengine.com>
2021-10-19 14:18:06 -0500

    Try simplifying testing env

--- plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
+++ plugins/wpe-headless/.docker/scripts/init-testing-environment.sh
@@ -4,21 +4,9 @@ cd /var/www/html/
 
 ls
 
-#1. check if wordpress is already installed/configured
-if (sudo -u www-data -- wp core is-installed)
-then
+cd wp-content/plugins/$WP_PLUGIN_FOLDER
 
-	#2. check if the database is ready
-	if ! (sudo -u www-data -- wp db check)
-	then
-		# wait a moment for the database container
-		sleep 1
-		exit 1;
-	fi
-
-	#3. init the testing instance
-	cd wp-content/plugins/$WP_PLUGIN_FOLDER && sudo -u www-data -- bash -c "./bin/install-wp-tests.sh $WP_TESTS_DB_NAME $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_DB_HOST latest"	
-fi
+bash -c "./bin/install-wp-tests.sh $WP_TESTS_DB_NAME $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_DB_HOST latest" 
 
 #4. back to the root WP folder
 cd /var/www/html/
