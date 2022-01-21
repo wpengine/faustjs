4d6e0081dd5566d13f6d3778b1136d169baaf5f0
Joe Fusco <joe.fusco@wpengine.com>
2022-01-21 10:48:20 -0500

    Validate nonce

--- plugins/faustwp/includes/deny-public-access/functions.php
+++ plugins/faustwp/includes/deny-public-access/functions.php
@@ -19,9 +19,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return bool
  */
 function doing_file_editor_save() {
-	if ( isset( $_REQUEST['wp_scrape_key'] ) && isset( $_REQUEST['wp_scrape_nonce'] ) ) {
-		return true;
+	if ( ! isset( $_REQUEST['wp_scrape_key'] ) || ! isset( $_REQUEST['wp_scrape_nonce'] ) ) {
+		return false;
 	}
 
-	return false;
+	$key   = substr( sanitize_key( wp_unslash( $_REQUEST['wp_scrape_key'] ) ), 0, 32 );
+	$nonce = wp_unslash( $_REQUEST['wp_scrape_nonce'] );
+
+	// Validate nonce.
+	if ( get_transient( 'scrape_key_' . $key ) !== $nonce ) {
+		return false;
+	}
+
+	return true;
 }
