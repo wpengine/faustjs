a2d0e1f91450955aa1cb7c42a5773a12549bec12
Joseph Fusco <josephfusco@users.noreply.github.com>
2022-01-26 11:27:42 -0500

    (style) add phpcs to lint workflow and address its warnings (#744)
    * phpcbf updates
    
    * Ignore warnings for checks used by WP core
    
    * Add missing doc comment
    
    * Use yoda condition
    
    * Add docs for $links param
    
    * Provide reason for disabling rule
    
    * Auto run PHPCS within pull requests
    
    * Constistent formatting in composer.json
    
    * Prevent allow-plugins CLI prompt for composer 2.2
    
    * Add full-stop after comment for linter
    
    * Include new line at end of file

--- plugins/faustwp/includes/deny-public-access/functions.php
+++ plugins/faustwp/includes/deny-public-access/functions.php
@@ -19,12 +19,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return bool
  */
 function doing_file_editor_save() {
+	// phpcs:disable WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	// Disabling as we are mimicking WordPress core's own check.
+	// https://github.com/WordPress/wordpress-develop/blob/5.8.1/src/wp-includes/load.php#L1591-L1595.
 	if ( ! isset( $_REQUEST['wp_scrape_key'] ) || ! isset( $_REQUEST['wp_scrape_nonce'] ) ) {
 		return false;
 	}
 
 	$key   = substr( sanitize_key( wp_unslash( $_REQUEST['wp_scrape_key'] ) ), 0, 32 );
 	$nonce = wp_unslash( $_REQUEST['wp_scrape_nonce'] );
+	// phpcs:enable WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 
 	// Validate nonce.
 	if ( get_transient( 'scrape_key_' . $key ) !== $nonce ) {
