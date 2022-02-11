d082c6adfc1326cbe714a2318f2fcca1ec461a21
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2022-02-11 16:44:41 +0000

    [MERL-94] Docs Explain Headless Settings in detail now (#767)
    * docs(faust.js) Add faustwp settings page and descriptions
    
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>
    
    * docs(faust.js) Update WordPress domain for media URL description
    
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>
    
    * docs(faust.js) Update wording in Disabling WordPress theme admin pages
    
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>
    
    * docs(settings.mdx): Code review fixes:
    * Replace `site_url` and `home_url`
    * Adds legend to settings fieldset with link to docs
    
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>
    
    * docs(settings.mdx): Code review fixes:
    * Replace rest of `home_url` with `WordPress URL`
    
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>

--- internal/website/sidebars.js
+++ internal/website/sidebars.js
@@ -209,6 +209,17 @@ module.exports = {
     },
     {
       type: 'category',
+      label: 'FaustWP Plugin',
+      items: [
+        {
+          type: 'doc',
+          label: 'Settings Reference',
+          id: 'faustwp/settings',
+        },
+      ],
+    },
+    {
+      type: 'category',
       label: 'Changelogs',
       items: [
         {
