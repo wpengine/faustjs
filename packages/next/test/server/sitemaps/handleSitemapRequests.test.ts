1433439e7d1be3e379648e6d11c120f78abd174d
Blake Wilson <blake.wilson@wpengine.com>
2022-03-01 14:57:07 -0600

    handleSitemapRequests tests

--- packages/next/test/server/sitemaps/handleSitemapRequests.test.ts
+++ packages/next/test/server/sitemaps/handleSitemapRequests.test.ts
@@ -1,3 +1,6 @@
+import { config as coreConfig } from '@faustjs/core';
+import { NextRequest } from 'next/server';
+import * as createSitemaps from '../../../src/server/sitemaps/createSitemaps';
 import * as handleSitemapRequests from '../../../src/server/sitemaps/handleSitemapRequests';
 
 describe('validateConfig', () => {
@@ -126,5 +129,151 @@ describe('validateConfig', () => {
     expect(() => handleSitemapRequests.validateConfig(config)).toThrow(
       'replaceUrls must be a boolean',
     );
+  });
+});
+
+describe('handleSitemapRequests', () => {
+  coreConfig({
+    wpUrl: 'http://headless.local',
+  });
+
+  it('properly routes to the sitemap index', async () => {
+    const createRootSitemapIndexSpy = jest
+      .spyOn(createSitemaps, 'createRootSitemapIndex')
+      .mockImplementation();
+
+    let res = {
+      url: 'http://localhost:3000/non-sitemap-route',
+    } as NextRequest;
+
+    let config: Partial<handleSitemapRequests.HandleSitemapRequestsConfig> = {
+      sitemapIndexPath: '/sitemap.xml',
+      replaceUrls: true,
+    };
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(createRootSitemapIndexSpy).not.toHaveBeenCalled();
+
+    res = {
+      url: 'http://localhost:3000/sitemap.xml',
+    } as NextRequest;
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(createRootSitemapIndexSpy).toHaveBeenCalledWith(res, config);
+  });
+
+  it('properly routes to the Faust pages sitemap', async () => {
+    const createPagesSitemapSpy = jest
+      .spyOn(createSitemaps, 'createPagesSitemap')
+      .mockImplementation();
+
+    let res = {
+      url: 'http://localhost:3000/non-sitemap-route',
+    } as NextRequest;
+
+    let config: Partial<handleSitemapRequests.HandleSitemapRequestsConfig> = {
+      sitemapIndexPath: '/sitemap.xml',
+      pages: [
+        {
+          path: '/about',
+        },
+      ],
+      replaceUrls: true,
+    };
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(createPagesSitemapSpy).not.toHaveBeenCalled();
+
+    res = {
+      url: 'http://localhost:3000/sitemap-faust-pages.xml',
+    } as NextRequest;
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(createPagesSitemapSpy).toHaveBeenCalledWith(res, config);
+  });
+
+  it('properly routes to a sitemap page', async () => {
+    const handleSitemapPathSpy = jest
+      .spyOn(createSitemaps, 'handleSitemapPath')
+      .mockImplementation();
+
+    let res = {
+      url: 'http://localhost:3000/non-sitemap-route',
+    } as NextRequest;
+
+    let config: Partial<handleSitemapRequests.HandleSitemapRequestsConfig> = {
+      sitemapIndexPath: '/sitemap.xml',
+      pages: [
+        {
+          path: '/about',
+        },
+      ],
+      replaceUrls: true,
+    };
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(handleSitemapPathSpy).not.toHaveBeenCalled();
+
+    res = {
+      url: 'http://localhost:3000/a-valid-sitemap-route.xml',
+    } as NextRequest;
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+
+    expect(handleSitemapPathSpy).toHaveBeenCalledWith(res, config);
+  });
+
+  it('does not return a response if the path is not a sitemap route', async () => {
+    const createRootSitemapIndexSpy = jest.spyOn(
+      createSitemaps,
+      'createRootSitemapIndex',
+    );
+    const createPagesSitemapSpy = jest.spyOn(
+      createSitemaps,
+      'createPagesSitemap',
+    );
+    const handleSitemapPathSpy = jest.spyOn(
+      createSitemaps,
+      'handleSitemapPath',
+    );
+
+    let config: Partial<handleSitemapRequests.HandleSitemapRequestsConfig> = {
+      sitemapIndexPath: '/sitemap.xml',
+      pages: [
+        {
+          path: '/about',
+        },
+      ],
+      replaceUrls: true,
+    };
+
+    // paths with "sitemap" should not be handled
+    let res = {
+      url: 'http://localhost:3000/my-sitemap',
+    } as NextRequest;
+
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+    expect(createRootSitemapIndexSpy).not.toHaveBeenCalled();
+    expect(createPagesSitemapSpy).not.toHaveBeenCalled();
+    expect(handleSitemapPathSpy).not.toHaveBeenCalled();
+
+    // Regular XML files should not be handled
+    (res as any).url = 'http://localhost:3000/an-xml-file.xml';
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+    expect(createRootSitemapIndexSpy).not.toHaveBeenCalled();
+    expect(createPagesSitemapSpy).not.toHaveBeenCalled();
+    expect(handleSitemapPathSpy).not.toHaveBeenCalled();
+
+    // path must end in .xml
+    (res as any).url = 'http://localhost:3000/my-sitemap.xml.txt';
+    await handleSitemapRequests.handleSitemapRequests(res, config);
+    expect(createRootSitemapIndexSpy).not.toHaveBeenCalled();
+    expect(createPagesSitemapSpy).not.toHaveBeenCalled();
+    expect(handleSitemapPathSpy).not.toHaveBeenCalled();
   });
 });
