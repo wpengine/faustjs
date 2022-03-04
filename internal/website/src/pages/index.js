f4bbae680f9adcb34c3d8e2d2ada158ce181e7a5
Kellen Mace <kellen.mace@gmail.com>
2022-03-04 12:34:27 -0500

    Revert "Add social media meta tags to Faust site"
    This reverts commit 37f60cdaa02ece6d66b698e8bbbafb89496f8705.

--- internal/website/src/pages/index.js
+++ internal/website/src/pages/index.js
@@ -7,7 +7,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/Features/HomepageFeatures';
 import HomepageGetStarted from '../components/GetStarted/HomepageGetStarted';
-import MetaTags from '../components/MetaTags';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -42,7 +41,6 @@ export default function Home() {
           httpEquiv="Content-Security-Policy"
           content="default-src 'self' http://kuergg39mb-dsn.algolia.net data: 'unsafe-inline' 'unsafe-eval'"
         />
-        <MetaTags />
       </Head>
       <Layout
         title={siteConfig.tagline}
