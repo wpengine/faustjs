c1a4060f3cb9950bd40102bbc1b0aba00493cb91
Blake Wilson <blake.wilson@wpengine.com>
2022-04-04 09:50:59 -0500

    Deploy Faust.js site to prod (#831)

--- internal/website/sidebars.js
+++ internal/website/sidebars.js
@@ -189,6 +189,11 @@ module.exports = {
             },
             {
               type: 'doc',
+              label: 'handleSitemapRequests',
+              id: 'next/reference/handle-sitemap-requests',
+            },
+            {
+              type: 'doc',
               label: 'URL Params',
               id: 'next/reference/expected-url-params',
             },
