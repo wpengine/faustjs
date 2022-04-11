0e57f304ffc1b0f9cfb497569ea697128252f9b2
Blake Wilson <blake.wilson@wpengine.com>
2022-04-11 10:59:00 -0500

    Sitemaps guide (#834)
    * Deploy Faust.js site to prod (#831)
    
    * Create a how-to guide for Faust.js Sitemaps
    
    Co-authored-by: Andrew Matthews <andrew.matthews@wpengine.com>

--- internal/website/sidebars.js
+++ internal/website/sidebars.js
@@ -124,6 +124,11 @@ module.exports = {
             },
             {
               type: 'doc',
+              label: 'Sitemaps',
+              id: 'next/guides/sitemaps',
+            },
+            {
+              type: 'doc',
               label: '404s',
               id: 'next/guides/handle-404s',
             },
