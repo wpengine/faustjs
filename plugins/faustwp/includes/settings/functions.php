70ea36ada4ada3f3666781952c5b99bbd13dcf57
Joe Fusco <joe.fusco@wpengine.com>
2022-05-02 14:26:15 -0400

    Fix linting error

--- plugins/faustwp/includes/settings/functions.php
+++ plugins/faustwp/includes/settings/functions.php
@@ -132,7 +132,6 @@ function maybe_set_default_settings() {
 
 		// Force WP to regenerate rewrite rules without calling flush_rewrite_rules which breaks
 		// things when used inside of `switch_to_blog()`.
-		// https://iandunn.name/2015/04/23/flushing-rewrite-rules-on-all-sites-in-a-multisite-network/
 		if ( is_multisite() ) {
 			delete_option( 'rewrite_rules' );
 		}
