9f16182cecccc50b727b09b61ab22e58cb3c4f71
theodesp <theo.despoudis@wpengine.com>
2022-06-07 16:02:08 +0100

    test(WIP): Test Add CustomPostTypeCest incrementally

--- plugins/faustwp/.docker/plugins/test-cpt/test-cpt.php
+++ plugins/faustwp/.docker/plugins/test-cpt/test-cpt.php
@@ -16,19 +16,12 @@ add_action( 'init', 'register_cpt' );
 function register_cpt() {
 	$args = array(
 		'label'               => 'Document',
-		'public'              => true,
-		'publicly_queryable'  => true,
-		'exclude_from_search' => false,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'has_archive'         => true,
-		'rewrite'             => true,
-		'query_var'           => true,
-		'show_in_rest'        => true,
-		'supports'            => array( 'editor' ),
 		'show_in_graphql'     => true,
 		'graphql_single_name' => 'document',
 		'graphql_plural_name' => 'documents',
+		'public'              => true,
+		'show_in_rest'        => true,
+		'supports'            => array( 'editor' ),
 	);
 	register_post_type( 'document', $args );
 }
