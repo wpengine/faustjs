433bd8794b9189ebdd171007d8105e28d703bc1d
theodesp <theo.despoudis@wpengine.com>
2022-06-07 15:44:03 +0100

    style(link): Fix PHPCS issues

--- plugins/faustwp/.docker/plugins/test-cpt/test-cpt.php
+++ plugins/faustwp/.docker/plugins/test-cpt/test-cpt.php
@@ -1,32 +1,34 @@
 <?php
-/*
-Plugin Name: Test CPT Plugin
-*/
+/**
+ * Plugin Name: Test CPT Plugin
+ *
+ * @package TestCPT
+ */
 
-// Exit if accessed directly.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action('init', 'register_cpt');
-function register_cpt()
-{
+add_action( 'init', 'register_cpt' );
+/**
+ * Register CPT
+ */
+function register_cpt() {
 	$args = array(
-		'label' => 'Document',
-		'public' => true,
-		'publicly_queryable' => true,
+		'label'               => 'Document',
+		'public'              => true,
+		'publicly_queryable'  => true,
 		'exclude_from_search' => false,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'has_archive' => true,
-		'rewrite' => true,
-		'query_var' => true,
-		'show_in_rest' => true,
-		'supports' => array('editor'),
-		'show_in_graphql' => true,
-		'hierarchical' => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+		'show_in_rest'        => true,
+		'supports'            => array( 'editor' ),
+		'show_in_graphql'     => true,
 		'graphql_single_name' => 'document',
 		'graphql_plural_name' => 'documents',
 	);
-	register_post_type('document', $args);
+	register_post_type( 'document', $args );
 }
