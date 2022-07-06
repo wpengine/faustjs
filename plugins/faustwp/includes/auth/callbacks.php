88ce018207feeff04754d9f4fea13729addcf642
Joseph Fusco <josephfusco@users.noreply.github.com>
2022-07-06 15:30:06 -0400

    Fix handling generate endpoint (#916)
    * support for sub-directory WP install
    
    * Add changeset
    
    * Add attribution to changelog
    
    Co-authored-by: kermage <genealyson.torcende@gmail.com>

--- plugins/faustwp/includes/auth/callbacks.php
+++ plugins/faustwp/includes/auth/callbacks.php
@@ -22,7 +22,9 @@ add_action( 'parse_request', __NAMESPACE__ . '\\handle_generate_endpoint' );
  * @return void
  */
 function handle_generate_endpoint() {
-	if ( ! preg_match( '/^\/generate/', $_SERVER['REQUEST_URI'] ) ) { // phpcs:ignore WordPress.Security
+	$search_pattern = ':^' . site_url( '/generate', 'relative' ) . ':';
+
+	if ( ! preg_match( $search_pattern, $_SERVER['REQUEST_URI'] ) ) { // phpcs:ignore WordPress.Security
 		return;
 	}
 
