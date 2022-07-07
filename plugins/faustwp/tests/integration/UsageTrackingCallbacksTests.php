1fc38b745689acaf268d02a975dee2c98af1ceb2
theodesp <328805+theodesp@users.noreply.github.com>
2022-07-07 14:05:57 +0100

    test(IntegrationTests): Fix correct action name

--- plugins/faustwp/tests/integration/UsageTrackingCallbacksTests.php
+++ plugins/faustwp/tests/integration/UsageTrackingCallbacksTests.php
@@ -13,7 +13,7 @@ class UsageTrackingCallbacksTests extends \WP_UnitTestCase {
 	}
 
 	public function test_the_callbacks_are_registered() {
-		$this->assertSame( 10, has_action( 'admin_notices', 'WPE\FaustWP\Usage_Tracking\show_prompt' ) );
+		$this->assertSame( 10, has_action( 'admin_notices', 'WPE\FaustWP\Usage_Tracking\show_usage_tracking_prompt' ) );
 		$this->assertSame( 10, has_action( 'admin_footer', 'WPE\FaustWP\Usage_Tracking\print_scripts' ) );
 		$this->assertSame( 10, has_action( 'wp_ajax_faustwp_plugin_usage_tracking', 'WPE\FaustWP\Usage_Tracking\ajax_maybe_dismiss_prompt' ) );
 	}
