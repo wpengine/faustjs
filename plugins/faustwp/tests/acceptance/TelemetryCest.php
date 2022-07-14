b54c047bdea7107e47d4869d3b6cc151b0e546bb
Joe Fusco <joe.fusco@wpengine.com>
2022-07-14 12:47:40 -0400

    Test test
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- plugins/faustwp/tests/acceptance/TelemetryCest.php
+++ plugins/faustwp/tests/acceptance/TelemetryCest.php
@@ -3,11 +3,18 @@
 class TelemetryCest
 {
 	/**
-	 * Ensure the nodejs site url is set as the post preview url.
+	 * Ensure the telemetry endpoint is accessible with the secret key.
 	 */
-	public function i_can_access_the_telemetry_endpoint(AcceptanceTester $I)
+	public function i_can_access_the_telemetry_endpoint_with_secret_key(AcceptanceTester $I)
 	{
+		$secret_key = '00000000-0000-4000-8000-000000000001';
+
 		$I->haveFaustWPSetting('frontend_uri', 'http://localhost:3000');
-		$I->haveFaustWPSetting('secret_key', '00000000-0000-4000-8000-000000000001');
+		$I->haveFaustWPSetting('secret_key', $secret_key);
+		$I->haveHttpHeader('x-faustwp-secret', $secret_key);
+		$I->sendGet('/wp-json/faustwp/v1/telemetry');
+		$I->seeResponseCodeIsSuccessful();
+		$I->seeResponseIsJson();
+		$I->seeResponseContains('{"faustwp":{"has_frontend_uri":true}}');
 	}
 }
