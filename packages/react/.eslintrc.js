4ddca1503e9befae986754c59cbaf2f3e87c6d68
Blake Wilson <blake@blake.id>
2022-07-20 10:58:59 -0500

    Revert "Delete eslint config files"
    This reverts commit cac774aabd40eb557263a59643245e89eb98b198.

---
+++ packages/react/.eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    amd: true,
+  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+      sourceType: 'module', // Allows for the use of imports
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'import', 'simple-import-sort'],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/unbound-method': 0,
+    'no-void': 0,
+    'import/named': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'jest.setup.ts',
+          '**/__tests__/*',
+          '**/*.test.ts',
+          '**/*.test.tsx',
+        ],
+      },
+    ],
+    'import/prefer-default-export': 0,
+    'react/jsx-closing-bracket-location': 0,
+    'react/jsx-props-no-spreading': ['error', { html: 'ignore' }],
+    'react/jsx-wrap-multilines': [
+      'error',
+      { declaration: false, assignment: false },
+    ],
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+    'import/extensions': ['error', 'always'],
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: 'React', // Pragma to use, default to "React"
+      version: '17.0', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+    },
+  },
+  ignorePatterns: [
+    'test/**/*',
+    '.prettierrc.js',
+    'jest.config.js',
+    'jest.setup.ts',
+    '.eslintrc.js',
+  ],
+};
