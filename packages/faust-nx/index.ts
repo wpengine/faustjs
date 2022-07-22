0f308c2e4ac961f490862ceb6773f124f54d9c7c
Blake Wilson <blake.wilson@wpengine.com>
2022-07-22 13:25:06 -0500

    Add exports to `faust-nx`

--- packages/faust-nx/index.ts
+++ packages/faust-nx/index.ts
@@ -2,6 +2,12 @@ import { FaustNXProvider } from './src/components/FaustNXProvider';
 import { WordPressTemplate } from './src/components/WordPressTemplate';
 import { getWordPressProps } from './src/getWordPressProps';
 import { getConfig, setConfig, FaustNXConfig } from './src/config/index.js';
+import { ensureAuthorization } from './src/auth/index.js';
+import {
+  authorizeHandler,
+  logoutHandler,
+  apiRouter,
+} from './src/server/index.js';
 
 export {
   FaustNXProvider,
@@ -10,4 +16,8 @@ export {
   getConfig,
   setConfig,
   FaustNXConfig,
+  ensureAuthorization,
+  authorizeHandler,
+  logoutHandler,
+  apiRouter,
 };
