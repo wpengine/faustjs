38d81d2ff1fc70b09c260d59cb5cedee07fc628f
Blake Wilson <blake.wilson@wpengine.com>
2022-07-28 14:51:54 -0500

    Fix CJS/MJS dist builds

---
+++ packages/faust-nx/src/index.ts
@@ -0,0 +1,19 @@
+import { FaustNXProvider } from './components/FaustNXProvider';
+import { WordPressTemplate } from './components/WordPressTemplate';
+import { getWordPressProps } from './getWordPressProps';
+import { getConfig, setConfig, FaustNXConfig } from './config/index.js';
+import { ensureAuthorization } from './auth/index.js';
+import { authorizeHandler, logoutHandler, apiRouter } from './server/index.js';
+
+export {
+  FaustNXProvider,
+  WordPressTemplate,
+  getWordPressProps,
+  getConfig,
+  setConfig,
+  FaustNXConfig,
+  ensureAuthorization,
+  authorizeHandler,
+  logoutHandler,
+  apiRouter,
+};
