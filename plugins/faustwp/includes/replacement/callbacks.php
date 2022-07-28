b169e71eba9113e4145322a82371785662494968
Joe Fusco <joe.fusco@wpengine.com>
2022-07-28 15:14:13 -0400

    Add the post/page URI to preview link
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- plugins/faustwp/includes/replacement/callbacks.php
+++ plugins/faustwp/includes/replacement/callbacks.php
@@ -161,6 +161,11 @@ function post_preview_link( $link, $post ) {
 			$args['typeName'] = $gql_type_name;
 		}
 
+		// Add the full URI of the post/page.
+		if ( ! isset( $args['uri'] ) ) {
+			$args['uri'] = urlencode( get_permalink( $post ) );
+		}
+
 		// Add ?p=xx&preview=true to link again.
 		$link = add_query_arg(
 			array(
