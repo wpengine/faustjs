b4abd7acbee0924302d8c82e70cfb58ecb0d11a4
Joe Fusco <joe.fusco@wpengine.com>
2022-07-28 20:34:47 -0400

    Export withFaust
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

---
+++ packages/faust-nx/src/index.ts
@@ -0,0 +1,21 @@
+import { FaustNXProvider } from './components/FaustNXProvider';
+import { WordPressTemplate } from './components/WordPressTemplate';
+import { getWordPressProps } from './getWordPressProps';
+import { getConfig, setConfig, FaustNXConfig } from './config/index.js';
+import { ensureAuthorization } from './auth/index.js';
+import { authorizeHandler, logoutHandler, apiRouter } from './server/index.js';
+import { withFaust } from './config/withFaust';
+
+export {
+  FaustNXProvider,
+  WordPressTemplate,
+  getWordPressProps,
+  getConfig,
+  setConfig,
+  FaustNXConfig,
+  ensureAuthorization,
+  authorizeHandler,
+  logoutHandler,
+  apiRouter,
+  withFaust
+};
