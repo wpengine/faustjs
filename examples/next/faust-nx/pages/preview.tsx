d4ecc0effea1973222c79fef4699ac8ebce1d927
Joe Fusco <joe.fusco@wpengine.com>
2022-08-02 12:01:13 -0400

    Scaffold preview
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faust-nx/pages/preview.tsx
+++ examples/next/faust-nx/pages/preview.tsx
@@ -1,5 +1,5 @@
 import 'faustnx.config';
-import { getWordPressProps, WordPressTemplate } from 'faust-nx';
+import { getWordPressProps, WordPressTemplate, usePreviewNode } from 'faust-nx';
 import { GetServerSidePropsContext } from 'next';
 import client from 'client';
 
@@ -7,28 +7,12 @@ export function getServerSideProps(ctx: GetServerSidePropsContext) {
   return getWordPressProps({ client, ctx });
 }
 
-export default function Preview() {
-  // const isLoading = client.useIsLoading();
-  // const { typeName, node } = client.auth.usePreviewNode();
+export default function Preview(props: any) {
+  const { node } = usePreviewNode();
 
-  // if (isLoading || node === undefined) {
-  //   return <p>Loading...</p>;
-  // }
+  if (node === null) {
+    return <p>Post not found</p>;
+  }
 
-  // if (node === null) {
-  //   return <p>Post not found</p>;
-  // }
-
-  // switch (typeName) {
-  //   case 'Page': {
-  //     return <WordPressTemplate {...props} />;
-  //   }
-  //   case 'Post': {
-  //     return 'post';
-  //   }
-  //   // Add custom post types here as needed
-  //   default: {
-  //     throw new Error(`Unknown post type: ${typeName}`);
-  //   }
-  // }
+  return <WordPressTemplate {...props} />;
 }
