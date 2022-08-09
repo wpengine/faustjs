4ab00ae63b83efe9ff8e01f05f335e29393189db
Matthew Wright <matthew.wright@wpengine.com>
2022-08-09 12:58:59 -0500

    placeholder

---
+++ examples/next/getting-started/src/pages/sitemap.xml.tsx
@@ -0,0 +1,21 @@
+import { handleSitemapRequests } from '@faustjs/next';
+
+export default function SitemapIndex() {}
+
+// getStaticProps - no context request object
+export const getServerSideProps = (ctx) => {
+    console.log(ctx.req.url);
+
+    if(ctx.req.url === '/sitemap.xml') {
+        // handlesitemaprequests.createrootsitemapindex()
+    }
+
+    if(ctx.req.url.includes('something')) {
+        // handlesitemaprequests.handlesitemappath()
+    }
+
+    return getServerSideSitemapIndex(ctx, [
+        'https://example.com/path-1.xml',
+        'https://example.com/path-2.xml',
+    ]);
+}
