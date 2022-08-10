526d30a02ce7ef7c3d4b2876581aaf062ac049c1
Matthew Wright <matthew.wright@wpengine.com>
2022-08-10 07:56:25 -0500

    Update sitemap.xml.tsx

--- examples/next/getting-started/src/pages/sitemap.xml.tsx
+++ examples/next/getting-started/src/pages/sitemap.xml.tsx
@@ -1,21 +1,27 @@
 import { handleSitemapRequests } from '@faustjs/next';
 
-export default function SitemapIndex() {}
+export default function SitemapIndex() {
+    return <div>Test</div>;
+}
 
 // getStaticProps - no context request object
 export const getServerSideProps = (ctx) => {
-    console.log(ctx.req.url);
+    // console.log(ctx.req.url);
 
-    if(ctx.req.url === '/sitemap.xml') {
-        // handlesitemaprequests.createrootsitemapindex()
-    }
+    // if(ctx.req.url === '/sitemap.xml') {
+    //     // handlesitemaprequests.createrootsitemapindex()
+    // }
 
-    if(ctx.req.url.includes('something')) {
-        // handlesitemaprequests.handlesitemappath()
-    }
+    // if(ctx.req.url.includes('something')) {
+    //     // handlesitemaprequests.handlesitemappath()
+    // }
 
-    return getServerSideSitemapIndex(ctx, [
-        'https://example.com/path-1.xml',
-        'https://example.com/path-2.xml',
-    ]);
+    // return getServerSideSitemapIndex(ctx, [
+    //     'https://example.com/path-1.xml',
+    //     'https://example.com/path-2.xml',
+    // ]);
+
+    return {
+        props: {}
+    };
 }
