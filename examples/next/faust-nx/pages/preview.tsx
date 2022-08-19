f0c49ea9a730fa68be34bb2b0e0e25c9bd28460f
Joe Fusco <joe.fusco@wpengine.com>
2022-08-19 10:50:29 -0400

    Clean up as preview are client side
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faust-nx/pages/preview.tsx
+++ examples/next/faust-nx/pages/preview.tsx
@@ -1,11 +1,5 @@
 import 'faustnx.config';
-import { getWordPressProps, WordPressTemplate } from 'faust-nx';
-import { GetServerSidePropsContext } from 'next';
-import client from 'client';
-
-export function getServerSideProps(ctx: GetServerSidePropsContext) {
-  return getWordPressProps({ client, ctx });
-}
+import { WordPressTemplate } from 'faust-nx';
 
 export default function Preview(props: any) {
   return <WordPressTemplate {...props} />;
