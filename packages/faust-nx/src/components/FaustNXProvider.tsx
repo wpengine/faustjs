97e3c40511dd303f0a000f32bb690768f0ca6283
Blake Wilson <blake.wilson@wpengine.com>
2022-08-22 15:18:31 -0500

    Make Apollo client configurable from the plugin system (#973)
    * Init Apollo client from the framework, added hooks to modify client
    
    * get apollo from getApolloClient()
    
    * Fix getProps

--- packages/faust-nx/src/components/FaustNXProvider.tsx
+++ packages/faust-nx/src/components/FaustNXProvider.tsx
@@ -1,20 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
-import {
-  ApolloClient,
-  ApolloProvider,
-  NormalizedCacheObject,
-} from '@apollo/client';
 // eslint-disable-next-line import/extensions
 import { AppProps } from 'next/app';
 import { useApollo } from '../client.js';
 
 export function FaustNXProvider(props: {
-  client: ApolloClient<NormalizedCacheObject>;
   children: React.ReactNode;
   pageProps: AppProps['pageProps'];
 }) {
-  const { client, pageProps, children } = props;
-  const apolloClient = useApollo(client, pageProps);
+  const { pageProps, children } = props;
+  const apolloClient = useApollo(pageProps);
 
   return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
 }
