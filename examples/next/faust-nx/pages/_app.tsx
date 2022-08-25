ecd6681b270e648ac9d75c813c0cfedc417affe3
Joe Fusco <joe.fusco@wpengine.com>
2022-08-25 08:54:16 -0400

    Add global styles
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faust-nx/pages/_app.tsx
+++ examples/next/faust-nx/pages/_app.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import client from 'client';
 import { FaustNXProvider } from 'faust-nx';
 import type { AppProps } from 'next/app';
+import 'styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
