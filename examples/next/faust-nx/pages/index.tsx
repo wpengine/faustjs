13d3a7bd8836da4f30ba470f049c8d4679ec89e6
Joe Fusco <joe.fusco@wpengine.com>
2022-08-25 08:54:51 -0400

    Scaffold nextjs page
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faust-nx/pages/index.tsx
+++ examples/next/faust-nx/pages/index.tsx
@@ -1,3 +1,18 @@
+import { Header, Footer, Container, Main } from "components";
+
 export default function Page() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <Container>
+          <>
+            <h1>Home</h1>
+            <p>This page is a traditional <a href="https://nextjs.org/docs/routing/introduction#index-routes">Next.js file-system based page</a>.</p>
+          </>
+        </Container>
+      </Main>
+      <Footer />
+    </>
+  );
 }
