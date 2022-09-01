2ce1d5bb2f4944a778cf6ede1e6783d57c6c8f1e
Blake Wilson <blake.wilson@wpengine.com>
2022-09-01 12:20:31 -0500

    Normalize config and require jest-environment-jsdom dep

--- packages/faust-nx/src/config/index.ts
+++ packages/faust-nx/src/config/index.ts
@@ -1,4 +1,7 @@
 import { PossibleTypesMap } from '@apollo/client';
+import defaults from 'lodash/defaults.js';
+import extend from 'lodash/extend.js';
+import isString from 'lodash/isString.js';
 import once from 'lodash/once.js';
 import { WordPressTemplate } from '../getWordPressProps.js';
 import { hooks, Plugin } from '../hooks/index.js';
@@ -25,6 +28,24 @@ export function setConfig(_config: FaustNXConfig) {
   })();
 }
 
+export function normalizeConfig(_config: FaustNXConfig): FaustNXConfig {
+  const cfg = defaults({}, _config, {
+    loginPagePath: '/login',
+    disableLogging: false,
+  });
+
+  Object.keys(cfg).forEach((key) => {
+    const keyValue: keyof FaustNXConfig = key as any;
+    const value = cfg[keyValue];
+
+    if (isString(value)) {
+      (cfg as any)[keyValue] = value.trim();
+    }
+  });
+
+  return extend(cfg, {});
+}
+
 export function getConfig(): Partial<FaustNXConfig> {
-  return config;
+  return normalizeConfig(config);
 }
