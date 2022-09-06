c38b62d6ac0d7806bf99fca075f77b695435a47e
Joe Fusco <joe.fusco@wpengine.com>
2022-09-06 17:17:54 -0400

    Revert "Remove jest env comment"
    This reverts commit 750af0f11f3b5aeb65183c485e632aa3583326dd.

--- packages/next/test/useCheckFaustContext.test.tsx
+++ packages/next/test/useCheckFaustContext.test.tsx
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { useCheckFaustContext } from "../src/gqty/hooks/useCheckFaustContext";
