80605bac606a5d351e1eb4cc4e9b7c5e92d41639
Joe Fusco <joe.fusco@wpengine.com>
2022-10-05 15:02:47 -0400

    Add generate function to CLI package
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

---
+++ packages/faustwp-cli/utils/generatePossibleTypes.ts
@@ -0,0 +1,52 @@
+import 'isomorphic-fetch';
+import fs from 'fs';
+
+import { infoLog, errorLog } from './log.js';
+
+export async function generatePossibleTypes() {
+  const GRAPHQL_ENDPOINT = `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql`;
+
+  const res = await fetch(GRAPHQL_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      variables: {},
+      query: `
+        {
+            __schema {
+                types {
+                    kind
+                    name
+                    possibleTypes {
+                       name
+                    }
+                }
+            }
+        }
+        `,
+    }),
+  });
+
+  const json = await res.json();
+
+  const possibleTypes: any = {};
+
+  json.data.__schema.types.forEach((supertype: any) => {
+    if (supertype.possibleTypes) {
+      possibleTypes[supertype.name] = supertype.possibleTypes.map(
+        (subtype: any) => subtype.name,
+      );
+    }
+  });
+
+  try {
+    fs.writeFileSync('./possibleTypes.json', JSON.stringify(possibleTypes));
+  } catch (err) {
+    errorLog('Could not generate possibleTypes.json', err);
+    process.exit(0);
+  }
+
+  infoLog('Updated possibleTypes.json');
+}
