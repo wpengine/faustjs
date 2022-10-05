0366c6ed2d7459716f92237905b79ee160ea2cb6
Joe Fusco <joe.fusco@wpengine.com>
2022-10-05 15:01:44 -0400

    Add generate function to CLI package
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/utils/index.ts
+++ packages/faustwp-cli/utils/index.ts
@@ -1,5 +1,6 @@
 import { getCliArgs } from './getCliArgs.js';
 import { errorLog, infoLog } from './log.js';
 import { validateFaustEnvVars } from './validateFaustEnvVars.js';
+import { generatePossibleTypes } from './generatePossibleTypes.js';
 
-export { getCliArgs, errorLog, infoLog, validateFaustEnvVars };
+export { getCliArgs, errorLog, infoLog, validateFaustEnvVars, generatePossibleTypes };
