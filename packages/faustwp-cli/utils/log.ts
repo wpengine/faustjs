332000ac6f269f608dd1b43aec243ae48f1579a1
Joe Fusco <josephfusco@users.noreply.github.com>
2022-10-07 11:17:03 -0400

    Add generatePossibleTypes script to `@faustwp/cli` (#1059)
    * Remove script from example project
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Use ‘info’ log level to match nodejs conventions
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add generate function to CLI package
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add generate function to CLI package
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Implement “generate” command
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add generate command to example project
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Remove unused default case
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add warnLog
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Use natural language in CLI
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Use script name in logs
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add full stop for all log messages
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Define isomorphic-fetch
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Fix linting errors
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Add eslint exception for __schema
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Linting fixes
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Update packages/faustwp-cli/utils/generatePossibleTypes.ts
    
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>
    
    * Update packages/faustwp-cli/utils/generatePossibleTypes.ts
    
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>
    
    * Reserve generate for future use
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Remove wp from public command
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    * Update CLI styles
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>

--- packages/faustwp-cli/utils/log.ts
+++ packages/faustwp-cli/utils/log.ts
@@ -1,32 +1,43 @@
-import chalk from 'chalk';
+import { styles } from './styles.js';
 
 export const log = (
-  logLevel: 'notice' | 'error',
+  logLevel: 'info' | 'warn' | 'error',
   message: string,
   ...args: any
 ) => {
-  let logLevelMessage = chalk.whiteBright('faust: ');
+  const delimeter = styles.dim(' - ');
+  let logLevelMessage = styles.brand('Faust') + delimeter;
 
   switch (logLevel) {
-    case 'notice': {
-      logLevelMessage += chalk.yellow('notice');
+    case 'info': {
+      logLevelMessage += styles.info('info');
+      break;
+    }
+    case 'warn': {
+      logLevelMessage += styles.warn('warn');
       break;
     }
     case 'error': {
-      logLevelMessage += chalk.red('error');
+      logLevelMessage += styles.error('error');
       break;
     }
     default: {
       break;
     }
   }
+
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, no-console
-  console.log(`${logLevelMessage} - ${message}`, ...args);
+  console.log(`${logLevelMessage}${delimeter}${message}`, ...args);
 };
 
-export const noticeLog = (message: string, ...args: any) => {
+export const infoLog = (message: string, ...args: any) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  log('notice', message, ...args);
+  log('info', message, ...args);
+};
+
+export const warnLog = (message: string, ...args: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  log('warn', message, ...args);
 };
 
 export const errorLog = (message: string, ...args: any) => {
