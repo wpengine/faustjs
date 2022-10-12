6c3edae9b9f069fe18abc30ef9753618e154ba72
Blake Wilson <blake.wilson@wpengine.com>
2022-10-12 08:37:29 -0500

    Revert "TESTING: new faust docs site build workflow"
    This reverts commit 235db2aa74da1a8e74c2de3ac7dcb871a6ee9777.

--- internal/faustjs.org/sidebars.js
+++ internal/faustjs.org/sidebars.js
@@ -65,7 +65,7 @@ module.exports = {
         {
           type: 'doc',
           label: 'Settings Reference',
-          id: 'faustwp/settingss',
+          id: 'faustwp/settings',
         },
       ],
     },
