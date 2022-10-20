c79883cff1641f5f31163fe42b34d9d260864b27
theodesp <328805+theodesp@users.noreply.github.com>
2022-10-20 17:27:17 +0100

    Chore: Remove omit helper

--- packages/faustwp-core/src/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-type Obj = { [k: string]: unknown };
-
-export function omit<T extends Obj>(obj: T, keys: string[]): Partial<Obj> {
-  return Object.fromEntries(
-    Object.entries(obj).filter(([k]) => !keys.includes(k)),
-  );
-}
--- packages/faustwp-core/src/utils/index.ts
+++ packages/faustwp-core/src/utils/index.ts
@@ -1,4 +1,3 @@
 export * from './assert.js';
 export * from './convert.js';
 export * from './log.js';
-export * from './helpers.js';
