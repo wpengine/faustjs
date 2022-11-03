8c0ead0c27019f54fa9c51683b3e4b7cdce403a6
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2022-11-02 19:02:01 -0500

    Removed cards with dead links from home page

--- internal/faustjs.org/src/components/Features/HomepageFeatures.js
+++ internal/faustjs.org/src/components/Features/HomepageFeatures.js
@@ -14,26 +14,6 @@ const FeatureList = [
     link: '/blog/the-future-of-faust#where-faustjs-is-going',
   },
   {
-    title: 'Next.js',
-    description: (
-      <>
-        Built on top of Next.js and React to take advantage of state-of-the-art
-        frontend tools
-      </>
-    ),
-    link: '/docs/next/getting-started',
-  },
-  {
-    title: 'GraphQL',
-    description: (
-      <>
-        Use GQty to fetch data from the WordPress GraphQL API without writing
-        GraphQL queries.
-      </>
-    ),
-    link: '/docs/next/guides/fetching-data',
-  },
-  {
     title: 'Previews',
     description: (
       <>
@@ -42,23 +22,6 @@ const FeatureList = [
       </>
     ),
     link: '/docs/next/guides/post-page-previews',
-  },
-  {
-    title: 'SSG and SSR',
-    description: (
-      <>
-        Easily render pages on the server, or generate static pages at
-        build-time.
-      </>
-    ),
-    link: '/docs/next/guides/ssr-ssg',
-  },
-  {
-    title: 'React Hooks',
-    description: (
-      <>Fetch posts, categories, pages, and more using standard URL params.</>
-    ),
-    link: '/docs/next/reference/hooks',
   },
   {
     title: 'WP Template Hierarchy',
@@ -73,16 +36,6 @@ const FeatureList = [
       <>Use plugins to add quick and flexible functionality to your product.</>
     ),
     link: '',
-  },
-  {
-    title: 'Custom Post Types',
-    description: (
-      <>
-        Add custom post types to your WordPress site and easily fetch them from
-        your frontend.
-      </>
-    ),
-    link: '/docs/next/guides/custom-post-types',
   },
 ];
 
