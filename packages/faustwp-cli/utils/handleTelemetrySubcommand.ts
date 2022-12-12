62c0f9e45bfacd85c341b37ffbc00d02a1bd382b
Joe Fusco <joe.fusco@wpengine.com>
2022-12-11 20:02:39 -0500

    Format
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/utils/handleTelemetrySubcommand.ts
+++ packages/faustwp-cli/utils/handleTelemetrySubcommand.ts
@@ -1,7 +1,7 @@
-import chalk from "chalk";
-import { getCliArgs } from "./getCliArgs.js";
-import { userConfig } from "./userConfig.js";
-import { styles } from "./styles.js";
+import chalk from 'chalk';
+import { getCliArgs } from './getCliArgs.js';
+import { userConfig } from './userConfig.js';
+import { styles } from './styles.js';
 
 const TELEMETRY_ENABLED = 'telemetry.enabled';
 const { log } = console;
@@ -9,59 +9,66 @@ const { log } = console;
 /**
  * Handles the telemetry subcommand.
  */
-export const handleTelemetrySubcommand = () => {
+export function handleTelemetrySubcommand() {
   const telemetryEnabled = userConfig.get(TELEMETRY_ENABLED);
   const args = getCliArgs();
   const subcommand = args[1];
 
-  switch(subcommand) {
+  switch (subcommand) {
     case 'enable': {
       userConfig.set(TELEMETRY_ENABLED, true);
       log(styles.success('Done!'));
-      printTelemetryStatus();
-      printThankYouMessage();
+      logTelemetryStatus();
+      log(
+        'Faust.js telemetry is completely anonymous. Thank you for participating!',
+      );
+      logTelemetryDocsUrl();
       break;
     }
 
     case 'disable': {
       telemetryEnabled
-        ? log(styles.success(`Your preference has been saved to ${userConfig.path}`))
-        : log(styles.warn(`Faust.js' telemetry collection is already disabled.`));
+        ? log(
+            styles.success(
+              `Your preference has been saved to ${userConfig.path}`,
+            ),
+          )
+        : log(
+            styles.warn(`Faust.js' telemetry collection is already disabled.`),
+          );
 
       userConfig.set(TELEMETRY_ENABLED, false);
-      printTelemetryStatus();
+      logTelemetryStatus();
       log("You have opted-out of Faust.js' anonymous telemetry program.");
-      log("No data will be collected from your machine.");
-      log('Learn more: https://faustjs.org/docs/telemetry');
+      log('No data will be collected from your machine.');
+      logTelemetryDocsUrl();
       break;
     }
 
     case 'status': {
       log(styles.brand('Faust.js Telemetry'));
-      printTelemetryStatus();
-      printThankYouMessage();
+      logTelemetryStatus();
+      log('Faust.js telemetry is completely anonymous.');
+      logTelemetryDocsUrl();
       break;
     }
 
     default: {
-      log(`"${subcommand}" is not a valid telemetry subcommand`);
+      log(styles.error(`"${subcommand}" is not a valid telemetry subcommand`));
       break;
     }
   }
-};
-
-const printTelemetryStatus = () => {
-  const statusSetting = userConfig.get(TELEMETRY_ENABLED);
-  const status = statusSetting
-    ? chalk.green('Enabled')
-    : chalk.red('Disabled');
-
-  log();
-  log(`Status: ${status}`);
-  log();
 }
 
-const printThankYouMessage = () => {
-  log('Faust.js telemetry is completely anonymous. Thank you for participating!');
+function logTelemetryStatus() {
+  const statusSetting = userConfig.get(TELEMETRY_ENABLED);
+  const status = statusSetting ? chalk.green('Enabled') : chalk.red('Disabled');
+
+  log('');
+  log(`Status: ${status}`);
+  log('');
+}
+
+function logTelemetryDocsUrl() {
   log('Learn more: https://faustjs.org/docs/telemetry');
 }
