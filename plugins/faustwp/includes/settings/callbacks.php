bbbcdf4bde6f110fd01fd342a5f27cf98ae94e22
Matthew Wright <matthew.wright@wpengine.com>
2023-01-03 15:55:02 -0600

    Update callbacks.php

--- plugins/faustwp/includes/settings/callbacks.php
+++ plugins/faustwp/includes/settings/callbacks.php
@@ -36,6 +36,7 @@ function frontend_url_notice() {
 				<p><?php esc_html_e( 'It is highly recommended that you update it below to avoid unexpected behavior.', 'faustwp' ); ?></p>
 				<p>
 					<?php
+					/* translators: %1$s: link start, %2$s: link end */
 					printf(
 						__( 'See the %1$s Getting Started Documentation%2$s for more details.', 'faustwp' ),
 						'<a href="https://faustjs.org/docs/getting-started" target="_blank" rel="noopener noreferrer">',
