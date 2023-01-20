0426b78a898244d3acd87732ebd264b68a2772d7
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2023-01-20 10:27:18 -0600

    Update packages/faustwp-cli/utils/shouldFireTelemetryEvent.ts
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>

--- packages/faustwp-cli/utils/shouldFireTelemetryEvent.ts
+++ packages/faustwp-cli/utils/shouldFireTelemetryEvent.ts
@@ -2,8 +2,6 @@ import { userConfig } from './userConfig.js';
 import { getWpSecret } from '../../faustwp-core/src/lib/getWpSecret.js';
 
 export function shouldFireTelemetryEvent(): boolean {
-  const args = getCliArgs();
-  const arg1 = args[0];
   const hasAnonymousId = userConfig.get('telemetry.anonymousId');
   const hasTelemetryEnabled = userConfig.get('telemetry.enabled') === true;
 
