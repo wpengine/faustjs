2a6886fac3659bd677d9c3d159e543645e4a6635
Joe Fusco <joe.fusco@wpengine.com>
2023-02-10 17:26:16 -0500

    Create core edit node
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

---
+++ packages/faustwp-core/src/components/Toolbar/nodes/Edit.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SeedNode } from '../../../queries/seedQuery.js';
+import { adminUrl } from '../../../utils/adminUrl.js';
+import { ToolbarLink } from '../index.js';
+
+export type EditProps = {
+  seedNode: SeedNode;
+};
+
+export function Edit({ seedNode }: EditProps) {
+  const postType = seedNode.__typename || '';
+  const postId = seedNode.databaseId || '';
+
+  const editPostUrl = adminUrl(`post.php?post=${postId}&action=edit`);
+
+  return <ToolbarLink url={editPostUrl}>Edit {postType}</ToolbarLink>;
+}
+
+Edit.id = 'edit';
