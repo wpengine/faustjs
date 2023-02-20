d85e180e5f24059a011968fe78d43209e00f78ac
Blake Wilson <blake.wilson@wpengine.com>
2023-02-20 12:27:46 -0600

    Test for `useLogout`

---
+++ packages/faustwp-core/tests/hooks/useLogout.test.ts
@@ -0,0 +1,89 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { renderHook } from '@testing-library/react-hooks';
+import fetchMock from 'fetch-mock';
+import { useLogout } from '../../src/hooks/useLogout';
+
+describe('useLogout hook', () => {
+  const windowBackup = window.location;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: jest.fn(), assign: jest.fn() },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: windowBackup,
+    });
+  });
+
+  it('has the proper initial state', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useLogout());
+
+    expect(result.current.error).toStrictEqual(undefined);
+    expect(result.current.loading).toStrictEqual(false);
+  });
+
+  it('calls the logout endpoint to clear the refresh token', async () => {
+    fetchMock.post(`/api/faust/auth/logout`, {
+      status: 205,
+    });
+
+    const { result } = renderHook(() => useLogout());
+
+    await result.current.logout();
+
+    expect(result.current.error).toStrictEqual(undefined);
+    expect(fetchMock.called()).toBeTruthy();
+
+    fetchMock.restore();
+  });
+
+  it('calls window.location.reload if no redirect url', async () => {
+    fetchMock.post(`/api/faust/auth/logout`, {
+      status: 205,
+    });
+
+    const { result } = renderHook(() => useLogout());
+
+    await result.current.logout();
+
+    expect(window.location.reload).toBeCalled();
+
+    fetchMock.restore();
+  });
+
+  it('calls window.location.assign if there is a redirect url', async () => {
+    fetchMock.post(`/api/faust/auth/logout`, {
+      status: 205,
+    });
+
+    const { result } = renderHook(() => useLogout());
+
+    await result.current.logout('/');
+
+    expect(window.location.assign).toBeCalledWith('/');
+
+    fetchMock.restore();
+  });
+
+  it('fails logout and provides the response object', async () => {
+    fetchMock.post(`/api/faust/auth/logout`, {
+      status: 500,
+    });
+
+    const { result } = renderHook(() => useLogout());
+
+    await result.current.logout();
+
+    expect(result.current.error).not.toBeUndefined();
+
+    fetchMock.restore();
+  });
+});
