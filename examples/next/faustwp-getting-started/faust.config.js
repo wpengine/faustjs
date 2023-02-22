20eeb969023369f842fafcd92a9b19711cdd3508
Joe Fusco <joe.fusco@wpengine.com>
2023-02-22 17:03:31 -0500

    Remove experimental toolbar from example project
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faustwp-getting-started/faust.config.js
+++ examples/next/faustwp-getting-started/faust.config.js
@@ -1,14 +1,12 @@
 import { setConfig } from '@faustwp/core';
 import templates from './wp-templates';
 import possibleTypes from './possibleTypes.json';
-import { CustomToolbar } from './plugins/CustomToolbar.tsx';
 
 /**
  * @type {import('@faustwp/core').FaustConfig}
  **/
 export default setConfig({
   templates,
-  experimentalPlugins: [new CustomToolbar()],
-  experimentalToolbar: true,
+  experimentalPlugins: [],
   possibleTypes,
 });
