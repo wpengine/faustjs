3c8e11bb6b70ca78cbbf3bd75a76e18de41207a2
Joe Fusco <joe.fusco@wpengine.com>
2023-02-24 12:37:36 -0500

    Add skeleton loader for individual toolbar nodes
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

---
+++ packages/faustwp-core/src/components/Toolbar/ToolbarNodeSkeleton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Skeleton component for a Toolbar Node.
+ */
+export function ToolbarNodeSkeleton() {
+  return (
+    <div className='toolbar-node-skeleton-wrapper'>
+      <div aria-hidden="true" className='toolbar-node-skeleton' />
+    </div>
+  );
+}
