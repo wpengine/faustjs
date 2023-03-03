90ec9e17ee393d388ef0e29ea29f39ad816bc7fd
theodesp <328805+theodesp@users.noreply.github.com>
2023-03-03 15:37:05 +0000

    Tests: Add tests for <Toolbar />

---
+++ packages/faustwp-core/tests/components/Toolbar/ToolbarItem.test.tsx
@@ -0,0 +1,11 @@
+/** @jest-environment jsdom */
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { ToolbarNode } from '../../../src/components/Toolbar';
+
+test('renders the component correctly', () => {
+  render(<ToolbarNode />);
+  const liElement = screen.getByRole(/listitem/i);
+  expect(liElement).toBeInTheDocument();
+});
