7a6f224add21ccb2d322a9198c4fb94ea8d7a8eb
Joe Fusco <joe.fusco@wpengine.com>
2023-03-14 18:15:32 -0400

    Update acceptance test
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- plugins/faustwp/tests/acceptance/PostPreviewCest.php
+++ plugins/faustwp/tests/acceptance/PostPreviewCest.php
@@ -7,18 +7,18 @@ class PostPreviewCest
      */
     public function i_can_view_the_post_preview_link(AcceptanceTester $I)
     {
-        $post_title = 'Post Preview Post';
-        $post_name = 'post-preview-post';
-		$post_content = 'Unpublished preview post content.';
+        $post_title    = 'Post Preview Post';
+        $post_name     = 'post-preview-post';
+        $post_content  = 'Unpublished preview post content.';
         $front_end_url = 'http://localhost:3000';
 
         $I->haveFaustWPSetting('frontend_uri', $front_end_url);
         $I->haveFaustWPSetting('secret_key', '00000000-0000-4000-8000-000000000001');
         $post_id = $I->havePostInDatabase([
-            'post_type' => 'post',
-            'post_title' => $post_title,
-			'post_content' => $post_content,
-            'post_name' => $post_name,
+            'post_type'    => 'post',
+            'post_title'   => $post_title,
+            'post_content' => $post_content,
+            'post_name'    => $post_name,
         ]);
 
         $I->loginAsAdmin();
@@ -30,10 +30,10 @@ class PostPreviewCest
             "${front_end_url}/${post_name}/?preview=true&previewPathname=" . rawurlencode("/post-preview-post/") . "&p=${post_id}&typeName=Post",
         );
 
-		$I->click('Preview in new tab');
-		$I->switchToNextTab();
-		$I->wait(14); // Wait for authentication
-        $I->see($post_title, 'section h1');
-		$I->see($post_content, 'main.content-single .wrap p');
+        $I->click('Preview in new tab');
+        $I->switchToNextTab();
+        $I->wait(14); // Wait for authentication
+        $I->see($post_title, 'main h1');
+        $I->see($post_content, '.entry-content');
     }
 }
