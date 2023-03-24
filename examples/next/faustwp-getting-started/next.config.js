75ded51874a563382421bf72ed82b1c3f7cb54fc
Joe Fusco <joe.fusco@wpengine.com>
2023-03-24 15:13:47 -0400

    Scaffold bundle analysis GitHub action
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faustwp-getting-started/next.config.js
+++ examples/next/faustwp-getting-started/next.config.js
@@ -1,9 +1,12 @@
 const { withFaust, getWpHostname } = require('@faustwp/core');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust({
+module.exports = withBundleAnalyzer(withFaust({
   reactStrictMode: true,
   sassOptions: {
     includePaths: ['node_modules'],
@@ -15,4 +18,4 @@ module.exports = withFaust({
     locales: ['en'],
     defaultLocale: 'en',
   },
-});
+}));
