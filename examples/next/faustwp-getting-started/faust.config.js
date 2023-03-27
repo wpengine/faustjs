1e54fffa9a31450d6b07001f273f57b9380548a6
Joe Fusco <joe.fusco@wpengine.com>
2023-03-27 10:50:48 -0400

    Test
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- examples/next/faustwp-getting-started/faust.config.js
+++ examples/next/faustwp-getting-started/faust.config.js
@@ -8,6 +8,6 @@ import possibleTypes from './possibleTypes.json';
 export default setConfig({
   templates,
   experimentalPlugins: [],
-  experimentalToolbar: true,
+  experimentalToolbar: false,
   possibleTypes,
 });
