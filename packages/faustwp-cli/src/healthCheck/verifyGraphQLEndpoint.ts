b0dcac944a17cf935c3d745f53c3ec50af9f4985
Joe Fusco <joe.fusco@wpengine.com>
2023-04-04 14:17:53 -0400

    Check status code
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/src/healthCheck/verifyGraphQLEndpoint.ts
+++ packages/faustwp-cli/src/healthCheck/verifyGraphQLEndpoint.ts
@@ -9,31 +9,13 @@ import { getGraphqlEndpoint } from '../utils/index.js';
 export async function verifyGraphQLEndpoint() {
   const graphqlEndpoint = getGraphqlEndpoint();
 
-  try {
-    const response: Response = await fetch(graphqlEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: '{ __typename }' }),
-    });
+  const response: Response = await fetch(graphqlEndpoint);
 
-    const json: {
-      data: {
-        __typename: 'string';
-      };
-    } = await response.json();
-
-    // eslint-disable-next-line no-underscore-dangle
-    if (json.data.__typename) {
-      infoLog('Discovered WPGraphQL endpoint!');
-      return true;
-    }
-
-    return false;
-  } catch (err) {
-    errorLog(`Unable to find a GraphQL endpoint at ${graphqlEndpoint}`);
-
-    process.exit(0);
+  if (200 === response.status) {
+    infoLog('Discovered WPGraphQL endpoint!');
+    return true;
   }
+
+  errorLog(`Unable to find a GraphQL endpoint at ${graphqlEndpoint}`);
+  return process.exit(0);
 }
