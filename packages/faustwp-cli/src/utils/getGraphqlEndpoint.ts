73472674ebe52f9b5d54e3a7f709e18a34907d3c
Joe Fusco <joe.fusco@wpengine.com>
2023-04-10 10:18:45 -0400

    Reference suggestion
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/src/utils/getGraphqlEndpoint.ts
+++ packages/faustwp-cli/src/utils/getGraphqlEndpoint.ts
@@ -6,5 +6,7 @@ import { getWpUrl } from './getWpUrl.js';
 export function getGraphqlEndpoint() {
   const wpUrl = getWpUrl();
 
+  // Use direct WordPress query to avoid relying on a filter.
+  // https://github.com/wpengine/faustjs/issues/1360
   return `${wpUrl}/index.php?graphql`;
 }
