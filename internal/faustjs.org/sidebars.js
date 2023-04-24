866b79260792c51c23a7b351e7ed7fb272d2ff92
Matthew Wright <matthew.wright@wpengine.com>
2023-04-24 12:51:59 -0500

    updated per pr

--- internal/faustjs.org/sidebars.js
+++ internal/faustjs.org/sidebars.js
@@ -60,6 +60,11 @@ module.exports = {
           label: 'Custom Post Types',
           id: 'guides/custom-post-types',
         },
+        {
+          type: 'doc',
+          label: 'Plugins',
+          id: 'guides/plugins',
+        },
       ],
     },
     {
