73570656e3dd693eff05e8ceec61578b5e0c6885
theodesp <328805+theodesp@users.noreply.github.com>
2023-05-10 17:15:39 +0100

    Refactor: Use getCSSRules instead of compileCSS

--- packages/blocks/src/utils/getInlineStyles.ts
+++ packages/blocks/src/utils/getInlineStyles.ts
@@ -1,44 +1,15 @@
-import isObject from 'lodash/isObject.js';
-import map from 'lodash/map.js';
-import camelCase from 'lodash/camelCase.js';
-import { compileCSS } from '@wordpress/style-engine';
+import { getCSSRules } from '@wordpress/style-engine';
 
-type Style = Parameters<typeof compileCSS>[0];
+type Style = Parameters<typeof getCSSRules>[0];
 
-const cssToReactStyle = (css: string | object): React.CSSProperties => {
-  // If object is given, return object (could be react style object mistakenly provided)
-  if (isObject(css)) {
-    return css;
-  }
-
-  // If falsy, then probably empty string or null, nothing to be done there
-  if (!css) {
-    return {};
-  }
-
-  // Only accepts strings
-  if (typeof css !== 'string') {
-    console.error(
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      `Unexpected type "${typeof css}" when expecting string, with value "${css}"`,
-    );
-    return {};
-  }
-
-  const style: Record<string, unknown> = {};
-  const rules = css.split(';');
-  map(rules, (rule) => {
-    let [key, value] = rule.split(':');
-
-    if (key && value) {
-      key = key.trim();
-      value = value.trim();
-      style[camelCase(key)] = value;
-    }
+export function parseInlineStyles(styles: Style): React.CSSProperties {
+  const output: Record<string, unknown> = {};
+  getCSSRules(styles).forEach((rule) => {
+    output[rule.key] = rule.value;
   });
 
-  return style;
-};
+  return output;
+}
 
 /**
  * Parses and returns any inline css styles on this block attributes
@@ -52,8 +23,8 @@ export default function getInlineStyles(
   // Duck Typing
   if (attributes?.style) {
     try {
-      styles = cssToReactStyle(
-        compileCSS(JSON.parse(attributes?.style as string) as Style),
+      styles = parseInlineStyles(
+        JSON.parse(attributes?.style as string) as Style,
       );
     } catch (e) {
       return styles;
