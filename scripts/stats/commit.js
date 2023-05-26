ab44a3c3a5b6879b6169a36067804ea854d365fd
Chris Wiegman <chris.wiegman@wpengine.com>
2023-05-26 13:46:08 -0400

    Finish creating contributor stats.

--- scripts/stats/commit.js
+++ scripts/stats/commit.js
@@ -1,39 +1,85 @@
-import { exec } from "child_process";
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
-const excludedContributors = [
-    'github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>',
-    'Theofanis Despoudis <328805+theodesp@users.noreply.github.com>',
-    'Teresa Gobble <teresagobble@gmail.com>',
-    'Joe Fusco <josephfusco@users.noreply.github.com>',
-    'Blake Wilson <blake.wilson@wpengine.com>' ,
-]
+const processExec = promisify(exec);
 
-exec('git shortlog -se --no-merges --since="28 days ago" < /dev/tty', {}, function (err, stdout, stderr) {
+const excludedFullContributors = [
+  'Theofanis Despoudis <328805+theodesp@users.noreply.github.com>',
+  'Teresa Gobble <teresagobble@gmail.com>',
+  'Joe Fusco <josephfusco@users.noreply.github.com>',
+  'Blake Wilson <blake.wilson@wpengine.com>',
+  'John Parris <john.parris@wpengine.com>',
+  'Blake Wilson <blake@blake.id>',
+  'John Parris <public@johnparris.com>',
+];
 
-    let totalContributions = {
-        'contributors': [],
-        'totals': {
-            'contributors': 0,
-            'contributions': 0,
+const excludedShortContributors = ['github-actions[bot]'];
+
+const publicRepos = [
+  'https://github.com/wpengine/wp-graphql-content-blocks.git',
+  'https://github.com/wpengine/faustjs.git',
+];
+
+async function getRepoStats() {
+  let repoStats = '';
+
+  for (const repo of publicRepos) {
+    const { stdout } = await processExec(
+      'git clone ' +
+        repo +
+        ' repo; cd repo; git shortlog -se --no-merges --since="28 days ago" < /dev/tty; cd ..; rm -rf repo',
+    );
+
+    repoStats += stdout;
+  }
+
+  return repoStats;
+}
+
+async function getContributorStats() {
+  let contributions = await getRepoStats();
+
+  let totalContributions = {
+    contributors: [],
+    totals: {
+      contributors: 0,
+      contributions: 0,
+    },
+  };
+
+  contributions.split('\n').forEach((contribution) => {
+    let contributor = contribution.trim().split('\t');
+
+    if (contributor.length == 2) {
+      let stat = {};
+      stat.count = parseInt(contributor[0]);
+      stat.user = contributor[1];
+
+      let goodUser = true;
+
+      if (excludedFullContributors.includes(stat.user)) {
+        goodUser = false;
+      }
+
+      excludedShortContributors.forEach((excludedContributor) => {
+        if (stat.user.includes(excludedContributor)) {
+          goodUser = false;
         }
+      });
+
+      if (goodUser) {
+        totalContributions.totals.contributors++;
+        totalContributions.totals.contributions += stat.count;
+        totalContributions.contributors.push(stat);
+      }
     }
+  });
 
-    arguments[1].split('\n').forEach(contributor => {
-        let contributions = contributor.trim().split('\t')
+  return totalContributions;
+}
 
-        if (contributions.length == 2) {
+(async () => {
+  let stats = await getContributorStats();
 
-            let contribution = {}
-            contribution.count = parseInt(contributions[0])
-            contribution.user = contributions[1]
-
-            if (!excludedContributors.includes(contribution.user)) {
-                totalContributions.totals.contributors++
-                totalContributions.totals.contributions += contribution.count
-                totalContributions.contributors.push(contribution)
-            }
-        }
-    });
-
-    console.log(totalContributions);
-});
+  console.log(stats);
+})();
