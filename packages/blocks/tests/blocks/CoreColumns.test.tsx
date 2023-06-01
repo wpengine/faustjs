583956114d8d158284c5fbbe9342839c2d97f454
theodesp <328805+theodesp@users.noreply.github.com>
2023-06-01 19:41:04 +0100

    Test: Fix CoreColumns inline snapshot

--- packages/blocks/tests/blocks/CoreColumns.test.tsx
+++ packages/blocks/tests/blocks/CoreColumns.test.tsx
@@ -37,12 +37,27 @@ describe('<CoreColumns />', () => {
 
   test('applies the correct styles', () => {
     renderProvider({
+      innerBlocks: [
+        {
+          name: 'CoreParagraph',
+        },
+      ],
       attributes: {
         cssClassName: 'has-background-color',
         style:
           '{"color":{"background":"#602929"},"typography":{"fontSize":"53px"}}',
       },
     });
-    expect(screen.queryByText('Hello World')).toMatchInlineSnapshot(`null`);
+    expect(screen.queryByText('Hello World')?.parentNode)
+      .toMatchInlineSnapshot(`
+      <div
+        class="has-background-color"
+        style="background-color: rgb(96, 41, 41); font-size: 53px;"
+      >
+        <div>
+          Hello World
+        </div>
+      </div>
+    `);
   });
 });
