23fd0673bd0ca65b6b7651cad40f46d8d6340ffd
Blake Wilson <blake.wilson@wpengine.com>
2023-06-05 08:46:45 -0500

    Core Code Block (#1441)
    * Feat: CoreParagraph Block WIP
    
    * Chore: Changeset
    
    * Test: Add CoreParagraph Tests
    
    * Added initial implementation + manual testing
    
    * Add tests
    
    * Remove testing changes
    
    * Import "blocks" styles
    
    * Update test
    
    ---------
    
    Co-authored-by: theodesp <328805+theodesp@users.noreply.github.com>

---
+++ packages/blocks/src/blocks/CoreCode.tsx
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+import React from 'react';
+import { useBlocksTheme } from '../components/WordPressBlocksProvider.js';
+import {
+  BlockWithAttributes,
+  ContentBlock,
+} from '../components/WordPressBlocksViewer.js';
+import { getStyles } from '../utils/index.js';
+
+export type CoreCodeFragmentProps = ContentBlock & {
+  attributes: {
+    anchor?: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    className?: string;
+    content?: string;
+    cssClassName?: string;
+    fontFamily?: string;
+    fontSize?: string;
+    gradient?: string;
+    lock?: string;
+    style?: string;
+    textColor?: string;
+  };
+};
+
+export function CoreCode(props: BlockWithAttributes<CoreCodeFragmentProps>) {
+  const theme = useBlocksTheme();
+  const style = getStyles(theme, { ...props });
+  const { attributes } = props;
+
+  return (
+    <pre style={style} className={attributes?.cssClassName}>
+      <code
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: attributes?.content ?? '' }}
+      />
+    </pre>
+  );
+}
+
+CoreCode.fragments = {
+  key: `CoreCodeBlockFragment`,
+  entry: gql`
+    fragment CoreCodeBlockFragment on CoreCode {
+      attributes {
+        anchor
+        backgroundColor
+        borderColor
+        className
+        content
+        cssClassName
+        fontFamily
+        fontSize
+        gradient
+        lock
+        style
+        textColor
+      }
+    }
+  `,
+};
--- packages/blocks/src/blocks/index.ts
+++ packages/blocks/src/blocks/index.ts
@@ -2,9 +2,11 @@
 import { CoreParagraph } from './CoreParagraph.js';
 import { CoreColumns } from './CoreColumns.js';
 import { CoreColumn } from './CoreColumn.js';
+import { CoreCode } from './CoreCode.js';
 
 export default {
   CoreParagraph: CoreParagraph,
   CoreColumns: CoreColumns,
   CoreColumn: CoreColumn,
+  CoreCode: CoreCode,
 };
