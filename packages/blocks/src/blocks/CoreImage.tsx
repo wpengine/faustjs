ef13d0552290407d0fc63afdefad428f71eb26d8
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-06-14 09:14:17 -0500

    CoreImage file added

---
+++ packages/blocks/src/blocks/CoreImage.tsx
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+import React from 'react';
+import { useBlocksTheme } from '../components/WordPressBlocksProvider.js';
+import {
+  BlockWithAttributes,
+  ContentBlock,
+} from '../components/WordPressBlocksViewer.js';
+import { getStyles } from '../utils/index.js';
+
+export type CoreImageFragmentProps = ContentBlock & {
+  attributes: {
+    align?: string;
+    alt?: string;
+    anchor?: string;
+    borderColor?: string;
+    caption?: string;
+    className?: string;
+    width?: string;
+    url?: string;
+    title?: string;
+    style?: string;
+    src?: string;
+    sizeSlug?: string;
+    rel?: string;
+    lock?: string;
+    linkTarget?: string;
+    linkDestination?: string;
+    linkClass?: string;
+    id?: string;
+    href?: string;
+    height?: string;
+    cssClassName?: string;
+  };
+};
+
+export function CoreImage(props: BlockWithAttributes<CoreImageFragmentProps>) {
+  const theme = useBlocksTheme();
+  const style = getStyles(theme, { ...props });
+  const { attributes } = props;
+
+  return (
+    <figure>
+      <img
+        style={style}
+        alt={attributes.alt}
+        className={attributes?.cssClassName}
+      />
+    </figure>
+  );
+}
+
+CoreImage.fragments = {
+  key: `CoreImageBlockFragment`,
+  entry: gql`
+    fragment CoreImageBlockFragment on CoreImage {
+      attributes {
+        align
+        alt
+        anchor
+        borderColor
+        caption
+        className
+        width
+        url
+        title
+        style
+        src
+        sizeSlug
+        rel
+        lock
+        linkTarget
+        linkDestination
+        linkClass
+        id
+        href
+        height
+        cssClassName
+      }
+    }
+  `,
+};
+
+CoreImage.config = {
+  name: 'CoreImage',
+};
+
+CoreImage.displayName = 'CoreImage';
