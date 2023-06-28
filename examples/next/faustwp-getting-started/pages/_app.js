86abc634f6165560d7e24b92e9c88f6f72eca4d3
theodesp <328805+theodesp@users.noreply.github.com>
2023-06-28 17:25:42 +0100

    Revert "Test: Testing Commit"
    This reverts commit 9dc1ba0a0c646b56d9dd3054e86e253a4f811977.

--- examples/next/faustwp-getting-started/pages/_app.js
+++ examples/next/faustwp-getting-started/pages/_app.js
@@ -3,21 +3,14 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
 import '@faustwp/core/dist/css/toolbar.css';
-import { WordPressBlocksProvider } from '@faustwp/blocks';
-import '../globalStylesheet.css';
-import blocks from '../wp-blocks';
+import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
   return (
     <FaustProvider pageProps={pageProps}>
-      <WordPressBlocksProvider
-        config={{
-          blocks,
-          theme: {},
-        }}>
-        <Component {...pageProps} key={router.asPath} />
-      </WordPressBlocksProvider>
+      <Component {...pageProps} key={router.asPath} />
     </FaustProvider>
   );
 }
--- examples/next/faustwp-getting-started/wp-templates/single.js
+++ examples/next/faustwp-getting-started/wp-templates/single.js
@@ -12,9 +12,6 @@ import {
   FeaturedImage,
   SEO,
 } from '../components';
-import components from '../wp-blocks';
-import { flatListToHierarchical } from '@faustwp/core';
-import { WordPressBlocksViewer } from '@faustwp/blocks';
 
 export default function Component(props) {
   // Loading state for previews
@@ -26,8 +23,7 @@ export default function Component(props) {
     props?.data?.generalSettings;
   const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
   const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
-  const { title, content, featuredImage, date, author, editorBlocks } = props.data.post;
-  const blocks = flatListToHierarchical(editorBlocks, {childrenKey: 'innerBlocks'});
+  const { title, content, featuredImage, date, author } = props.data.post;
 
   return (
     <>
@@ -49,10 +45,8 @@ export default function Component(props) {
             date={date}
             author={author?.node?.name}
           />
-          <Container className="wp-block-group is-layout-flow">
-            <ContentWrapper className="entry-content wp-block-post-content has-global-padding is-layout-constrained">
-              <WordPressBlocksViewer blocks={blocks}/>
-            </ContentWrapper>
+          <Container>
+            <ContentWrapper content={content} />
           </Container>
         </>
       </Main>
@@ -65,7 +59,6 @@ Component.query = gql`
   ${BlogInfoFragment}
   ${NavigationMenu.fragments.entry}
   ${FeaturedImage.fragments.entry}
-  ${components.CoreHeading.fragments.entry}
   query GetPost(
     $databaseId: ID!
     $headerLocation: MenuLocationEnum
@@ -80,14 +73,6 @@ Component.query = gql`
         node {
           name
         }
-      }
-      editorBlocks {
-        name
-        __typename
-        renderedHtml
-        id: clientId
-        parentId: parentClientId
-        ...${components.CoreHeading.fragments.key}
       }
       ...FeaturedImageFragment
     }
