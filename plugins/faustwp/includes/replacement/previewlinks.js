0d145f80bbf40fb591e109a9169094bda16d08f3
Matthew Wright <matthew.wright@wpengine.com>
2023-07-25 11:13:31 -0500

    updated to show preview link in dropdown menu

--- plugins/faustwp/includes/replacement/previewlinks.js
+++ plugins/faustwp/includes/replacement/previewlinks.js
@@ -18,38 +18,26 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     const intervalId = setInterval(function () {
-      const previewButton = jQuery(
-        'button[class~="block-editor-post-preview__button-toggle"]',
+      const wpPreviewLink = jQuery(
+        'a[class~="edit-post-header-preview__button-external"]',
       );
 
-      if (!previewButton.length) {
+      if (!wpPreviewLink.length) {
         return;
       }
 
       clearInterval(intervalId);
-      previewButton.first().one('click', function () {
-        setTimeout(function () {
-          const links = jQuery('a[target*="wp-preview"]');
 
-          if (!links.length) {
-            return;
-          }
-
-          links.each((i, link) => {
-            link.href = previewLink;
-
-            var copy = link.cloneNode(true);
-            copy.addEventListener('click', function () {
-              previewButton[0].click();
-
-              wp.data.dispatch('core/editor').autosave();
-            });
-
-            link.parentElement.insertBefore(copy, link);
-            link.style.display = 'none';
-          });
-        }, 100);
+      var faustPreviewLink = wpPreviewLink.clone();
+      faustPreviewLink.href = previewLink;
+      faustPreviewLink.html(
+        `Preview Headless site <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M18.2 17c0 .7-.6 1.2-1.2 1.2H7c-.7 0-1.2-.6-1.2-1.2V7c0-.7.6-1.2 1.2-1.2h3.2V4.2H7C5.5 4.2 4.2 5.5 4.2 7v10c0 1.5 1.2 2.8 2.8 2.8h10c1.5 0 2.8-1.2 2.8-2.8v-3.6h-1.5V17zM14.9 3v1.5h3.7l-6.4 6.4 1.1 1.1 6.4-6.4v3.7h1.5V3h-6.3z"></path></svg>`,
+      );
+      faustPreviewLink[0].addEventListener('click', function () {
+        wp.data.dispatch('core/editor').autosave();
       });
+      faustPreviewLink.insertAfter(wpPreviewLink);
+      faustPreviewLink.parent().css('display', 'block');
     }, 100);
   });
 });
