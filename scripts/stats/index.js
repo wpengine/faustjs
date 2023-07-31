9e0514a8e83dcca85ec36a3162022c64ea266b3c
Chris Wiegman <chris.wiegman@wpengine.com>
2023-07-31 12:01:19 -0400

    Minor simplification of stats calls.

--- scripts/stats/index.js
+++ scripts/stats/index.js
@@ -4,11 +4,8 @@ import * as downloads from "./src/downloads.js"
 (async () => {
     let stats = {}
 
-    const contributorStats = await contributors.getStats();
-    const downloadStats = await downloads.getStats();
-
-    stats.contributors = contributorStats
-    stats.downloads = downloadStats;
+    stats.contributors = await contributors.getStats();
+    stats.downloads = await downloads.getStats();
 
     console.log(stats);
 })();
