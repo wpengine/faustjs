46424a978e2d5b8e44537b0383332980a0e3edb7
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2023-08-08 15:54:38 +0100

    Project: ✨ New Package @block-editor-utils (#1516)
    * Project: ✨ New Package @block-editor
    
    * Update packages/block-editor/LICENSE
    
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>
    
    * Refactor: Rename package to block-editor-utils
    
    * Docs: Add Readme
    
    * Chore: Update package-lock.
    
    ---------
    
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>

---
+++ packages/block-editor-utils/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+
+  // Adds Jest support for TypeScript using ts-jest.
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  // Run code before each file in the suite is tested.
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // ESM Support
+  // @link https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  passWithNoTests: true,
+};
