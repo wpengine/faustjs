3fe60becfc77e98343252309227972062e71bf59
Blake Wilson <blake.wilson@wpengine.com>
2023-08-09 23:56:17 -0500

    Clean up tests

--- packages/experimental-app-router/tests/client.test.tsx
+++ packages/experimental-app-router/tests/client.test.tsx
@@ -3,9 +3,6 @@
  */
 
 import * as apolloClient from '@apollo/client';
-import * as ApolloAppSupportSSR from '@apollo/experimental-nextjs-app-support/ssr';
-import { setConfig } from '@faustwp/core';
-import React from 'react';
 import * as client from '../src/client';
 
 afterEach(() => {
@@ -20,7 +17,6 @@ describe('getClient', () => {
   beforeEach(() => {
     process.env = { ...envBackup };
     // @ts-ignore
-    React.createContext = () => {};
   });
 
   afterEach(() => {
@@ -40,26 +36,6 @@ describe('getClient', () => {
 
     expect(httpLinkSpy).toHaveBeenCalledWith({
       uri: 'http://headless.local/index.php?graphql',
-    });
-  });
-
-  it('creates the client with the appropriate possible types', async () => {
-    const apolloClientSpy = jest.spyOn(
-      ApolloAppSupportSSR,
-      'NextSSRApolloClient',
-    );
-
-    process.env.NEXT_PUBLIC_WORDPRESS_URL = 'http://headless.local';
-    setConfig({
-      possibleTypes: { test: ['test'] },
-      templates: {},
-      experimentalPlugins: [],
-    });
-
-    client.getClient();
-
-    expect(apolloClientSpy).toHaveBeenCalledWith({
-      test: 'test',
     });
   });
 });
