c8d98e3fb6b3116c893c7f60e1d15f64f0aa7b53
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-08-29 14:47:27 -0500

    Added preview typeName query to edit functionality
    Co-authored-by: Joe Fusco <josephfusco@users.noreply.github.com>
    Co-authored-by: Blake Wilson <blake@blake.id>

--- packages/faustwp-core/src/components/Toolbar/nodes/Edit.tsx
+++ packages/faustwp-core/src/components/Toolbar/nodes/Edit.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router.js';
 import { SeedNode } from '../../../queries/seedQuery.js';
 import { getAdminUrl } from '../../../lib/getAdminUrl.js';
 import { ToolbarItem } from '../index.js';
@@ -8,17 +9,23 @@ type Props = {
 };
 
 export function Edit({ seedNode }: Props) {
-  if (
-    seedNode === undefined ||
-    seedNode?.isFrontPage ||
-    seedNode?.isPostsPage
-  ) {
+  const {
+    query: { p, typeName },
+  } = useRouter();
+
+  if (seedNode?.isFrontPage || seedNode?.isPostsPage) {
     return null;
   }
 
   // eslint-disable-next-line no-underscore-dangle
-  const postType = seedNode?.__typename || '';
-  const postId = seedNode?.databaseId || '';
+  let postType = seedNode?.__typename || '';
+  let postId = seedNode?.databaseId || '';
+
+  // This is a preview.
+  if (!postId) {
+    postId = p;
+    postType = typeName;
+  }
 
   const editPostUrl = getAdminUrl(`post.php?post=${postId}&action=edit`);
 
