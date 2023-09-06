57fa72c8b4b401409a82f74104c5996aab1d7b4e
Blake Wilson <blake.wilson@wpengine.com>
2023-09-06 09:06:26 -0500

    fetchAccessToken comments

--- packages/experimental-app-router/src/server/auth/fetchAccessToken.ts
+++ packages/experimental-app-router/src/server/auth/fetchAccessToken.ts
@@ -1,5 +1,12 @@
 import { fetchTokens } from './fetchTokens.js';
 
+/**
+ * Fetches an access token from the token endpoint. Uses fetchTokens under
+ * the hood.
+ *
+ * @param code string|undefined An authorization code to get tokens.
+ * @returns string|null
+ */
 export async function fetchAccessToken(code?: string) {
   const tokens = await fetchTokens(code);
 
