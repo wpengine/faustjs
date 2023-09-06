71fa0850c105638c2c5cf84a8467ba6fc0188766
Blake Wilson <blake.wilson@wpengine.com>
2023-09-06 09:00:49 -0500

    Remove unneeded import

--- packages/experimental-app-router/src/server/routeHandler/tokenHandler.ts
+++ packages/experimental-app-router/src/server/routeHandler/tokenHandler.ts
@@ -2,7 +2,6 @@ import { getWpUrl } from '@faustwp/core/dist/cjs/lib/getWpUrl.js';
 import { getWpSecret } from '@faustwp/core/dist/cjs/lib/getWpSecret.js';
 import { cookies } from 'next/headers.js';
 import { NextResponse } from 'next/server.js';
-import { setRefreshToken } from '../../server-actions/utils/setRefreshToken';
 
 export type AuthorizeResponse = {
   accessToken: string;
