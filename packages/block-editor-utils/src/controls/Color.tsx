295e5c5c43aa5ad6c8d201ae59c2a5d12eeafe4e
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-09-06 14:48:29 -0500

    Fixed linting errors

--- packages/block-editor-utils/src/controls/Color.tsx
+++ packages/block-editor-utils/src/controls/Color.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import { ColorPicker, ColorIndicator } from '@wordpress/components';
+import { ColorPicker } from '@wordpress/components';
 import { ControlProps } from '../types/index.js';
 
-
-// following the convention set in Text.tsx
 function Color<T extends Record<string, any>>({
   config,
   props,
@@ -11,17 +9,19 @@ function Color<T extends Record<string, any>>({
   const onChange = (newContent: string) => {
     props.setAttributes({ [config.name]: newContent });
   };
-  // check for default value (https://developer.wordpress.org/block-editor/reference-guides/components/color-picker/#usage:~:text=setColor%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20enableAlpha-,defaultValue%3D%22%23000%22,-/%3E%0A%20%20%20%20)%3B)
-  // label https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label
-  
+
   return (
     <>
-    <label style={{ marginBottom: "10px", display: "block" }} htmlFor={config.label}>{config.label}</label>  
+      <label
+        style={{ marginBottom: '10px', display: 'block' }}
+        htmlFor={config.label}>
+        {config.label}
+      </label>
       <ColorPicker
         id={config.label}
         color={props.attributes[config.name]}
         onChange={onChange}
-      />  
+      />
     </>
   );
 }
