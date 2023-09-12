527d0cf50c0ef2b623d65a46960c20c10e5d20da
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-09-12 13:49:36 -0500

    Added Checkbox to registerControl index

--- packages/block-editor-utils/src/controls/index.ts
+++ packages/block-editor-utils/src/controls/index.ts
@@ -2,7 +2,9 @@ import registerControl from '../helpers/registerControl.js';
 import Text from './Text.js';
 import NumberField from './Number.js';
 import Color from './Color.js';
+import Checkbox from './Checkbox.js';
 
 registerControl('text', Text);
 registerControl('number', NumberField);
 registerControl('color', Color);
+registerControl('checkbox', Checkbox);
