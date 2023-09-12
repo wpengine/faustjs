adb21b166e0e6d95fd9b0cc6a981148a2b924b1b
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-09-12 15:47:18 -0500

    Updated unit testing

--- packages/block-editor-utils/tests/controls/Checkbox.test.tsx
+++ packages/block-editor-utils/tests/controls/Checkbox.test.tsx
@@ -1,67 +1,59 @@
-// import React from 'react';
-// import { screen, render } from '@testing-library/react';
-// import Checkbox from '../../src/controls/Checkbox';
-// import { Field } from '../../src/types';
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import Checkbox from '../../src/controls/Checkbox';
+import { Field } from '../../src/types';
 
-// Object.defineProperty(window, 'matchMedia', {
-//     writable: true,
-//     value: jest.fn().mockImplementation(query => ({
-//       matches: false,
-//       media: query,
-//       onchange: null,
-//       addListener: jest.fn(), // Deprecated
-//       removeListener: jest.fn(), // Deprecated
-//       addEventListener: jest.fn(),
-//       removeEventListener: jest.fn(),
-//       dispatchEvent: jest.fn(),
-//     })),
-//   });
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 
-// const config: Field = {
-//   label: 'radioField',
-//   name: 'radioField',
-//   type: 'string',
-//   control: 'radio',
-//   location: 'inspector',
-//   options: [
-//     {
-//       label: 'Red',
-//       value: 'red',
-//     },
-//     {
-//       label: 'Blue',
-//       value: 'blue',
-//     },
-//   ],
-// };
-// const props = {
-//   setAttributes: jest.fn(),
-//   attributes: {
-//     radioField: 'red',
-//   },
-// };
+const config: Field = {
+  location: "inspector",
+  control: "checkbox",
+  label: 'checkboxField', 
+  default: "false",
+  help: "here is help",
+  name: "checkboxField",
+  type: "boolean"
+};
+const props = {
+  setAttributes: jest.fn(),
+  attributes: {
+    checkboxField: "1",
+  },
+};
 
-// const setup = () => {
-//   const utils = render(<Checkbox config={config} props={props} />);
-//   const input = screen.findAllByDisplayValue(props.attributes.radioField!);
-//   return {
-//     input,
-//     ...utils,
-//   };
-// };
+const setup = () => {
+  const utils = render(<Checkbox config={config} props={props} />);
+  const input = screen.findAllByDisplayValue(props.attributes.checkboxField!);
+  return {
+    input,
+    ...utils,
+  };
+};
 
-// afterEach(() => {
-//   jest.clearAllMocks();
-// });
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-// describe('Checkbox', () => {
-//   it('should mount', () => {
-//     const { input } = setup();
-//     expect(input).toBeTruthy();
-//   });
+describe('Checkbox', () => {
+  it('should mount', () => {
+    const { input } = setup();
+    expect(input).toBeTruthy();
+  });
 
-//   it('should have correct display value from props', () => {
-//     setup();
-//     expect(screen.getByDisplayValue(props.attributes.radioField)).toBeTruthy();
-//   });
-// });
+  it('should have correct display value from props', () => {
+    setup();
+    expect(screen.getByDisplayValue(props.attributes.checkboxField)).toBeTruthy();
+  });
+});
