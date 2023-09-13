bbc69fa45413d0c5793c151a59c91ba444eff5ea
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-09-13 18:54:49 -0500

    Clean up testing file

--- packages/block-editor-utils/tests/controls/Checkbox.test.tsx
+++ packages/block-editor-utils/tests/controls/Checkbox.test.tsx
@@ -3,36 +3,36 @@ import { screen, render, fireEvent } from '@testing-library/react';
 import Checkbox from '../../src/controls/Checkbox';
 import { Field } from '../../src/types';
 import { useState } from 'react';
-import '@testing-library/jest-dom'; 
+import '@testing-library/jest-dom';
 
 Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // Deprecated
-      removeListener: jest.fn(), // Deprecated
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 const config: Field = {
-  location: "inspector",
-  control: "checkbox",
-  label: 'checkboxField', 
-  default: "false",
-  name: "checkboxField",
-  type: "boolean"
+  location: 'inspector',
+  control: 'checkbox',
+  label: 'checkboxField',
+  default: 'false',
+  name: 'checkboxField',
+  type: 'boolean',
 };
 
 // This should set the checkbox value (aka check the box)
 const props = {
   setAttributes: jest.fn(),
   attributes: {
-    checkboxField: "1",
+    checkboxField: '1',
   },
 };
 
@@ -49,71 +49,13 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-
-// FireEvent for click
-// fireEvent.change(getByLabelText(/username/i), {target: {value: 'a'}})
-
-//fire event:
-// const Button = ({onClick, children}) => (
-//   <button onClick={onClick}>{children}</button>
-// )
-
-// test('calls onClick prop when clicked', () => {
-//   const handleClick = jest.fn()
-//   render(<Button onClick={handleClick}>Click Me</Button>)
-//   fireEvent.click(screen.getByText(/click me/i))
-//   expect(handleClick).toHaveBeenCalledTimes(1)
-// })
-
-// describe('Checkbox is checked', () => {
-//   test('should pass', () => {
-//     const Wrap = () => {
-//       const [isChecked, setIsChecked] = useState(false);
-//       return <CheckBox isChecked={isChecked} onChange={() => setIsChecked(!isChecked)} />;
-//     };
-//     const { container } = render(<Wrap />);
-//     const checkbox = container.querySelectorAll("input[type='checkbox']")[0] as HTMLInputElement;
-//     fireEvent.click(checkbox);
-//     expect(checkbox.checked).toBe(true);
-//   });
-// });
-
 describe('Checkbox', () => {
   it('should mount', () => {
     const { input } = setup();
     expect(input).toBeTruthy();
   });
 
-  // it('should pass', () => {
-  //   const Wrap = () => {
-  //     const [isChecked, setIsChecked] = useState(false);
-  //     return <Checkbox isChecked={isChecked} onChange={() => setIsChecked(!isChecked)} />;
-  //   };
-  //   const { container } = render(<Wrap />);
-  //   const checkbox = container.querySelectorAll("input[type='checkbox']")[0] as HTMLInputElement;
-  //   fireEvent.click(checkbox);
-  //   expect(checkbox.checked).toBe(true);
-  // });
-
-//   (method) TestingLibraryMatchers<(str: string) => any, void>.toBeChecked(): void
-// @description
-// Assert whether the given element is checked.
-
-// It accepts an input of type checkbox or radio and elements with a role of radio with a valid aria-checked attribute of "true" or "false".
-
-// @example
-
-// <input
-//   type="checkbox"
-//   checked
-//   data-testid="input-checkbox" />
-
-// const inputCheckbox = getByTestId('input-checkbox')
-
-// expect(inputCheckbox).toBeChecked()
-// expect(inputRadio).not.toBeChecked()
-
-  it("checks that checkbox gets checked", () => {
+  it('checks that checkbox gets checked', () => {
     render(
       <input
         type="checkbox"
@@ -121,10 +63,10 @@ describe('Checkbox', () => {
         name="checkbox-array"
         value="second"
         id="second"
-      />
+      />,
     );
 
-    const checkbox = screen.getByRole("checkbox", { name: "checkbox" });
+    const checkbox = screen.getByRole('checkbox', { name: 'checkbox' });
     expect(checkbox).not.toBeChecked();
     fireEvent.click(checkbox);
     expect(checkbox).toBeChecked();
@@ -132,6 +74,8 @@ describe('Checkbox', () => {
 
   it('should have correct display value from props', () => {
     setup();
-    expect(screen.getByDisplayValue(props.attributes.checkboxField)).toBeTruthy();
+    expect(
+      screen.getByDisplayValue(props.attributes.checkboxField),
+    ).toBeTruthy();
   });
 });
