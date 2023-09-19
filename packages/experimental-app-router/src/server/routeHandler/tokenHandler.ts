507cc4e0d79d1b76758e9a66c594fd892a05689d
Blake Wilson <blake.wilson@wpengine.com>
2023-09-19 11:05:42 -0500

    Make use of deep paths more clear in app router package (#1581)
    * Only use esm
    
    * Use mjs imports instead of cjs
    
    * Use mjs version of config
    
    * Use `ApolloClient`/`InMemoryCache`
    
    * Add changesets
    
    * Use `isolatedModules`. Thanks @theodesp !
    
    * Make the temp use of deep imports more clear

--- packages/experimental-app-router/src/server/routeHandler/tokenHandler.ts
+++ packages/experimental-app-router/src/server/routeHandler/tokenHandler.ts
@@ -1,7 +1,6 @@
-import { getWpUrl } from '@faustwp/core/dist/mjs/lib/getWpUrl.js';
-import { getWpSecret } from '@faustwp/core/dist/mjs/lib/getWpSecret.js';
 import { cookies } from 'next/headers.js';
 import { NextResponse } from 'next/server.js';
+import { getWpUrl, getWpSecret } from '../../faust-core-utils.js';
 
 export type AuthorizeResponse = {
   accessToken: string;
