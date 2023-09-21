b9ba52457b3079ac94eef5949e601b3f5552e657
Joe Fusco <joe.fusco@wpengine.com>
2023-09-21 11:52:26 -0400

    Refactor into testable functions
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/src/blockset.ts
+++ packages/faustwp-cli/src/blockset.ts
@@ -7,7 +7,6 @@ import archiver from 'archiver';
 
 import { getWpUrl, getWpSecret } from './utils/index.js';
 
-// Define directories and paths
 const rootDir: string = process.cwd();
 const faustDir: string = path.join(rootDir, '.faust');
 const blocksDir: string = path.join(faustDir, 'blocks');
@@ -22,68 +21,91 @@ const manifest: { blocks: any[]; timestamp: string } = {
   timestamp: new Date().toISOString(),
 };
 
+/**
+ * Fetch block.json files, ignoring node_modules.
+ */
+async function fetchBlockFiles(): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    glob(`${rootDir}/**/block.json`, { ignore: '**/node_modules/**' }, (err, files) => {
+      if (err) reject(err);
+      else resolve(files);
+    });
+  });
+}
+
+/**
+ * Process each block.json file.
+ */
+async function processBlockFiles(files: string[]): Promise<void> {
+  for (const filePath of files) {
+    const blockDir = path.dirname(filePath);
+    const blockName = path.basename(blockDir);
+    const destDir = path.join(blocksDir, blockName);
+
+    await fs.copy(blockDir, destDir);
+
+    const blockJson = await fs.readJson(filePath);
+    manifest.blocks.push(blockJson);
+  }
+}
+
+/**
+ * Create a ZIP archive of the blocks.
+ */
+async function createZipArchive(): Promise<string> {
+  const zipPath = path.join(faustDir, 'blocks.zip');
+  const output = fs.createWriteStream(zipPath);
+  const archive = archiver('zip');
+
+  archive.pipe(output);
+  archive.directory(blocksDir, false);
+  await archive.finalize();
+
+  return zipPath;
+}
+
+/**
+ * Upload the ZIP archive to WordPress.
+ */
+async function uploadToWordPress(zipPath: string): Promise<void> {
+  const form = new FormData();
+  form.append('zipfile', fs.createReadStream(zipPath));
+
+  const apiUrl = `${getWpUrl()}/wp-json/faustwp/v1/blockset`;
+  const headers = {
+    ...form.getHeaders(),
+    'x-faustwp-secret': getWpSecret() || '',
+  };
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    body: form as any,
+    headers,
+  });
+
+  const responseText = await response.text();
+  console.log("Raw response:", responseText);
+
+  if (response.ok) {
+    try {
+      const data = await response.json();
+      console.log('Successfully uploaded to WordPress:', data);
+    } catch (error) {
+      console.error("Error parsing response as JSON:", error);
+    }
+  } else {
+    const errorData = await response.text();
+    throw new Error(`Error uploading to WordPress: ${errorData}`);
+  }
+}
+
 export async function blockset(): Promise<void> {
   try {
-    // Function to find and process block.json files
-    glob(`${rootDir}/**/block.json`, async (err: Error | null, files: string[]) => {
-      if (err) {
-        throw new Error(`Error while searching for block.json files: ${err}`);
-      }
-
-      // Loop through each block.json file
-      for (const filePath of files) {
-        const blockDir: string = path.dirname(filePath);
-        const blockName: string = path.basename(blockDir);
-        const destDir: string = path.join(blocksDir, blockName);
-
-        // Copy block directory to .faust/blocks
-        await fs.copy(blockDir, destDir);
-
-        // Read block.json and add to manifest
-        const blockJson: any = await fs.readJson(filePath);
-        manifest.blocks.push(blockJson);
-      }
-
-      // Write manifest.json
-      await fs.writeJson(manifestPath, manifest, { spaces: 2 });
-
-      // Create ZIP archive of .faust/blocks
-      const archive = archiver('zip');
-      const zipPath: string = path.join(faustDir, 'blocks.zip');
-      const output = fs.createWriteStream(zipPath);
-
-      archive.pipe(output);
-      archive.directory(blocksDir, false);
-      await archive.finalize();
-
-      // Prepare form data for POST request
-      const form = new FormData();
-      form.append('zipfile', fs.createReadStream(zipPath));
-
-      // Make a POST request to the WordPress REST endpoint
-      const wpUrl = getWpUrl();
-      const apiUrl: string = `${wpUrl}/wp-json/faustwp/v1/blockset`;
-
-      // Merge custom headers with form headers
-      const headers = {
-        ...form.getHeaders(),
-        'x-faustwp-secret': getWpSecret() || '',
-      };
-
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        body: form as any,
-        headers,
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Successfully uploaded to WordPress:', data);
-      } else {
-        const errorData = await response.text();
-        throw new Error(`Error uploading to WordPress: ${errorData}`);
-      }
-    });
+    const files = await fetchBlockFiles();
+    await processBlockFiles(files);
+    await fs.writeJson(manifestPath, manifest, { spaces: 2 });
+    const zipPath = await createZipArchive();
+    await uploadToWordPress(zipPath);
   } catch (error) {
     console.error(`"faust blockset" failed with the following error: `, error);
     process.exit(0);
