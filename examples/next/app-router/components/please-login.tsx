79fc4f6e86e624a293b482fb8e779e2152ec71d7
Blake Wilson <blake.wilson@wpengine.com>
2023-09-22 10:51:33 -0500

    Update examples/next/app-router/components/please-login.tsx
    Co-authored-by: Teresa Gobble <teresagobble@gmail.com>

--- examples/next/app-router/components/please-login.tsx
+++ examples/next/app-router/components/please-login.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export function PleaseLogin() {
   return (
     <>
-      You must be authenticated! Please <Link href={`/login`}>login</Link>{' '}
+      You must be authenticated! Please <Link href={`/login`}>log in</Link>{' '}
       first.
     </>
   );
