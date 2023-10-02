d6f410e0d784f1462632d51917c53f51a19b2262
Chris Wiegman <chris.wiegman@wpengine.com>
2023-10-02 08:40:14 -0400

    Collect total contributors

--- scripts/stats/src/contributors.js
+++ scripts/stats/src/contributors.js
@@ -3,49 +3,113 @@ import { promisify } from 'util';
 
 const processExec = promisify(exec);
 
-const excludedFullContributors = [
-  'Theofanis Despoudis <328805+theodesp@users.noreply.github.com>',
-  'Teresa Gobble <teresagobble@gmail.com>',
-  'Joe Fusco <josephfusco@users.noreply.github.com>',
-  'Blake Wilson <blake.wilson@wpengine.com>',
-  'John Parris <john.parris@wpengine.com>',
-  'Blake Wilson <blake@blake.id>',
-  'John Parris <public@johnparris.com>',
+const excludedShortContributors = [
+  'github-actions[bot]',
+  'Chris Wiegman',
+  'dependabot[bot]',
+  'Matthew Wright',
+  'William Johnston',
+  'wpe-jenkins-github-admin',
+  'Nick Cernis',
+  'Matt Landers',
+  'Mark Kelnar',
+  'Kellen Mace',
+  'wpengine.com',
+  'Will Johnston ',
+  'Joseph Fusco',
+  'Theofanis Despoudis',
+  'Teresa Gobble',
+  'Jordan Maslyn',
+  'Joe Fusco',
+  'Fran Agulto',
+  'Diego Gallardo',
+  'Brandon DuRette',
+  'theodesp',
+  'Clay Griffiths',
+  'Blake Wilson',
+  'Jason Bahl',
+  'John Parris',
 ];
-
-const excludedShortContributors = ['github-actions[bot]', 'Chris Wiegman', 'dependabot[bot]'];
 
 const publicRepos = [
   'https://github.com/wpengine/wp-graphql-content-blocks.git',
   'https://github.com/wpengine/faustjs.git',
 ];
 
+const contributorStats = {
+  individuals: [],
+  totals: {
+    contributors: 0,
+    contributions: 0,
+  }
+}
+
 async function getRepoLogs() {
-  let repoStats = '';
+  let repoStats = {
+    lastMonth: '',
+    allTime: ''
+  };
 
   for (const repo of publicRepos) {
-    const { stdout } = await processExec(
-      'git clone ' +
-      repo +
-      ' repo; cd repo; git shortlog -se --no-merges --since="28 days ago" < /dev/tty; cd ..; rm -rf repo',
-    );
 
-    repoStats += stdout;
+    let commands = [
+      {
+        command: 'git clone ' +
+          repo +
+          ' repo'
+      },
+      {
+        name: "lastMonth",
+        command: 'cd repo; git shortlog -se --no-merges --since="28 days ago" < /dev/tty'
+      },
+      {
+        name: "allTime",
+        command: 'cd repo; git shortlog -se --no-merges < /dev/tty'
+      },
+      {
+        command: 'rm -rf repo'
+      },
+    ]
+
+    for (const command of commands) {
+      let output = await runCommand(command.command);
+
+      if (command.name == 'lastMonth') {
+        repoStats.lastMonth += output
+      }
+
+      if (command.name == 'allTime') {
+        repoStats.allTime += output
+      }
+    }
   }
 
   return repoStats;
+}
+
+async function runCommand(command) {
+  const { stdout } = await processExec(command);
+
+  return stdout
 }
 
 export async function getStats() {
   let contributions = await getRepoLogs();
 
   let totalContributions = {
-    individuals: [],
-    totals: {
-      contributors: 0,
-      contributions: 0,
-    },
+    lastMonth: contributorStats,
+    allTime: contributorStats,
+    withWPE: contributorStats,
   };
+
+  totalContributions.lastMonth = await crunchContributions(contributions.lastMonth)
+  totalContributions.allTime = await crunchContributions(contributions.allTime)
+
+  return totalContributions;
+}
+
+async function crunchContributions(contributions) {
+  let stats = contributorStats
 
   contributions.split('\n').forEach((contribution) => {
     let contributor = contribution.trim().split('\t');
@@ -57,10 +121,6 @@ export async function getStats() {
 
       let goodUser = true;
 
-      if (excludedFullContributors.includes(stat.user)) {
-        goodUser = false;
-      }
-
       excludedShortContributors.forEach((excludedContributor) => {
         if (stat.user.includes(excludedContributor)) {
           goodUser = false;
@@ -68,12 +128,12 @@ export async function getStats() {
       });
 
       if (goodUser) {
-        totalContributions.totals.contributors++;
-        totalContributions.totals.contributions += stat.count;
-        totalContributions.individuals.push(stat);
+        stats.totals.contributors++;
+        stats.totals.contributions += stat.count;
+        stats.individuals.push(stat);
       }
     }
   });
 
-  return totalContributions;
+  return stats
 }
