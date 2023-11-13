03b36c028481300696cc32996f53048ffe749dea
theodesp <328805+theodesp@users.noreply.github.com>
2023-11-13 13:47:33 +0000

    Fix: (faustwp) Fix autosave in preview links.

--- plugins/faustwp/includes/replacement/previewlinks.js
+++ plugins/faustwp/includes/replacement/previewlinks.js
@@ -2,7 +2,7 @@
  * XXX: Please remove this once this issue is resolved: https://github.com/WordPress/gutenberg/issues/13998
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Get the preview data via wp_localize_script
   const faustPreviewData = window._faustwp_preview_data;
 
@@ -19,12 +19,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function debounce(func, wait) {
     let timeout;
-    return function() {
-      const context = this;
-      const args = arguments;
+    return function (...args) {
       clearTimeout(timeout);
-      timeout = setTimeout(function() {
-        func.apply(context, args);
+      timeout = setTimeout(function () {
+        func(args);
       }, wait);
     };
   }
@@ -34,25 +32,51 @@ document.addEventListener('DOMContentLoaded', function() {
     switch (version) {
       default:
         return {
-          headerLink: document.querySelector('.edit-post-header-preview__grouping-external a'),
-          snackbarLink: document.querySelector('.components-snackbar__content a'),
+          headerLink: document.querySelector(
+            '.edit-post-header-preview__grouping-external a',
+          ),
+          snackbarLink: document.querySelector(
+            '.components-snackbar__content a',
+          ),
         };
+    }
+  }
+
+  function autoSave() {
+    wp.data.dispatch('core/editor').autosave();
+  }
+
+  function maybeSetAutoUpdate() {
+    const { headerLink, snackbarLink } = getPreviewLinksByVersion(wpVersion);
+    if (headerLink) {
+      if (!headerLink.onClick) {
+        headerLink.onClick = autoSave;
+        headerLink.onTouch = autoSave;
+      }
+    }
+    if (snackbarLink) {
+      if (!snackbarLink.onClick) {
+        snackbarLink.onClick = autoSave;
+        snackbarLink.onTouch = autoSave;
+      }
     }
   }
 
   function updateUIElements() {
     const { headerLink, snackbarLink } = getPreviewLinksByVersion(wpVersion);
 
-    // Clone & replace the original link in order to clear pre-existing events.
     if (headerLink && headerLink.getAttribute('href') !== faustPreviewLink) {
-      const clonedHeaderLink = headerLink.cloneNode(true);
-      headerLink.parentNode.replaceChild(clonedHeaderLink, headerLink);
-      if (clonedHeaderLink) clonedHeaderLink.setAttribute('href', faustPreviewLink);
+      headerLink.setAttribute('href', faustPreviewLink);
     }
 
-    if (snackbarLink && snackbarLink.getAttribute('href') !== faustPreviewLink) {
+    if (
+      snackbarLink &&
+      snackbarLink.getAttribute('href') !== faustPreviewLink
+    ) {
       snackbarLink.setAttribute('href', faustPreviewLink);
     }
+
+    maybeSetAutoUpdate();
   }
 
   // Run the update function on initial page load.
