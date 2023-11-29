93f9085e87eb5a220e8b0378fa2a6d313599ed4c
Joe Fusco <joe.fusco@wpengine.com>
2023-11-29 14:43:56 -0500

    Handle PHP block files in the CLI w/ tests
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/faustwp-cli/src/blockset.ts
+++ packages/faustwp-cli/src/blockset.ts
@@ -7,19 +7,24 @@ import archiver from 'archiver';
 import { spawnSync } from 'child_process';
 
 import { getWpUrl, getWpSecret, hasYarn } from './utils/index.js';
-import { infoLog } from './stdout/index.js';
+import { infoLog, debugLog } from './stdout/index.js';
 
-const ROOT_DIR = process.cwd();
-const FAUST_DIR = path.join(ROOT_DIR, '.faust');
-const FAUST_BUILD_DIR = path.join(FAUST_DIR, 'build');
-const BLOCKS_DIR = path.join(FAUST_DIR, 'blocks');
-const MANIFEST_PATH = path.join(BLOCKS_DIR, 'manifest.json');
+// File paths used throughout the blockset process
+export const ROOT_DIR = process.cwd();
+export const FAUST_DIR = path.join(ROOT_DIR, '.faust');
+export const FAUST_BUILD_DIR = path.join(FAUST_DIR, 'build');
+export const BLOCKS_DIR = path.join(FAUST_DIR, 'blocks');
+export const MANIFEST_PATH = path.join(BLOCKS_DIR, 'manifest.json');
+
 const IGNORE_NODE_MODULES = '**/node_modules/**';
 const FAUST_BLOCKS_SRC_DIR = 'wp-blocks';
 
-// Ensure required directories exist
+// Ensure that the required directories for block processing exist
 fs.ensureDirSync(BLOCKS_DIR);
 
+/**
+ * Represents the structure of the manifest file.
+ */
 export type Manifest = {
   blocks: any[];
   timestamp: string;
@@ -31,9 +36,50 @@ const manifest: Manifest = {
 };
 
 /**
+ * Interface representing the structure of the parsed PHP asset file.
+ */
+export interface PhpAsset {
+  dependencies?: string[];
+  version?: string;
+}
+
+/**
+ * Parses a PHP asset file content and converts it into a JSON object.
+ * 
+ * @param {string} phpContent - The content of the PHP asset file.
+ * @returns {PhpAsset} - A JSON object representing the parsed content.
+ */
+export function parsePhpAssetFile(phpContent: string): PhpAsset {
+  const jsonObject: PhpAsset = {};
+
+  // Match the PHP array structure
+  const matches = /return\s+array\(([^;]+)\);/.exec(phpContent);
+  if (!matches || matches.length < 2) {
+    console.error('Error: Unable to parse PHP file.');
+    return {};
+  }
+
+  // Extract dependencies if present
+  const dependenciesMatch = matches[1].match(/'dependencies'\s*=>\s*array\(([^)]+)\)/);
+  if (dependenciesMatch) {
+    jsonObject.dependencies = dependenciesMatch[1]
+      .split(',')
+      .map(dep => dep.trim().replace(/'/g, ''));
+  }
+
+  // Extract version if present
+  const versionMatch = matches[1].match(/'version'\s*=>\s*'([^']+)'/);
+  if (versionMatch) {
+    jsonObject.version = versionMatch[1];
+  }
+
+  return jsonObject;
+}
+
+/**
  * Fetches paths to all block.json files while ignoring node_modules.
  *
- * @returns {Promise<string[]>} An array of paths to block.json files.
+ * @returns {Promise<string[]>} - An array of paths to block.json files.
  */
 export async function fetchBlockFiles(): Promise<string[]> {
   return glob(`${FAUST_BUILD_DIR}/**/block.json`, {
@@ -42,14 +88,14 @@ export async function fetchBlockFiles(): Promise<string[]> {
 }
 
 /**
- * Processes each block.json file, copying its directory and updating the manifest.
+ * Processes each block.json file by copying its directory, updating the manifest,
+ * and handling PHP files.
  *
  * @param {string[]} files - An array of paths to block.json files.
  * @returns {Promise<void>}
  */
 export async function processBlockFiles(files: string[]): Promise<void> {
   await fs.emptyDir(BLOCKS_DIR);
-  // Use Promise.all and map instead of for...of loop
   await Promise.all(
     files.map(async (filePath) => {
       const blockDir = path.dirname(filePath);
@@ -58,8 +104,34 @@ export async function processBlockFiles(files: string[]): Promise<void> {
 
       await fs.copy(blockDir, destDir);
 
-      const blockJson = await fs.readJson(filePath);
-      manifest.blocks.push(blockJson);
+      // Check if the file is a JSON file.
+      if (path.extname(filePath) === '.json') {
+        try {
+          const blockJson = await fs.readJson(filePath);
+          manifest.blocks.push(blockJson);
+        } catch (error) {
+          console.error(`Error reading JSON file: ${filePath}`, error);
+        }
+      }
+
+      // Handle PHP asset file.
+      const phpAssetPath = path.join(blockDir, 'index.asset.php');
+      if (await fs.pathExists(phpAssetPath)) {
+        const phpContent = await fs.readFile(phpAssetPath, 'utf8');
+        const assetData = parsePhpAssetFile(phpContent);
+        await fs.writeJson(path.join(destDir, 'index.asset.json'), assetData, { spaces: 2 });
+        await fs.remove(phpAssetPath);
+      }
+
+      // Check for and remove any other PHP files as many hosts do not allow *.php files
+      // within the uploads directory which could throw an error during the blockset process.
+      const phpFiles = await glob(`${destDir}/**/*.php`, {
+        ignore: IGNORE_NODE_MODULES,
+      });
+      for (const file of phpFiles) {
+        debugLog(`Removing PHP file: ${file}`);
+        await fs.remove(file);
+      }
     }),
   );
 }
@@ -67,7 +139,7 @@ export async function processBlockFiles(files: string[]): Promise<void> {
 /**
  * Creates a ZIP archive of the blocks.
  *
- * @returns {Promise<string>} Path to the created ZIP archive.
+ * @returns {Promise<string>} - Path to the created ZIP archive.
  */
 export async function createZipArchive(): Promise<string> {
   const zipPath = path.join(FAUST_DIR, 'blocks.zip');
@@ -133,12 +205,12 @@ export async function uploadToWordPress(zipPath: string): Promise<void> {
 }
 
 /**
- * Compiles the blocks and places them into the faust build dir.
+ * Compiles the blocks and places them into the FAUST build directory.
  *
  * @returns {Promise<void>}
  */
 export async function compileBlocks(): Promise<void> {
-  infoLog(`Faust: Compiling Blocks into ${FAUST_BUILD_DIR}`);
+  debugLog(`Faust: Compiling Blocks into ${FAUST_BUILD_DIR}`);
   await fs.emptyDir(FAUST_BUILD_DIR);
   const command = hasYarn() ? 'yarn' : 'npm';
   let args = ['exec', 'wp-scripts', 'start', '--package=@wordpress/scripts'];
