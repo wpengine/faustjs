50e5bc44bce26926f2ddc8e60f63b5ee43aaf867
Matthew Wright <matthew.wright@wpengine.com>
2023-12-12 10:41:40 -0600

    remove unneeded files

--- packages/faustwp-cli/src/telemetry/doTelemetryPrefsExist.ts
+++
@@ -1,9 +0,0 @@
-import { userConfig } from '../userConfig.js';
-
-export function telemetryPrefsExist(): boolean {
-  return (
-    userConfig.all.telemetry !== undefined &&
-    userConfig.all.telemetry.anonymousId !== undefined &&
-    userConfig.all.telemetry.enabled !== undefined
-  );
-}
