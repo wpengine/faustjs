b665744d7f4040bb1de8c01d0d3f58ce969d091e
Blake Wilson <blake.wilson@wpengine.com>
2023-12-14 10:25:03 -0600

    Create process telemetry rest route

--- plugins/faustwp/includes/telemetry/functions.php
+++ plugins/faustwp/includes/telemetry/functions.php
@@ -103,3 +103,18 @@ function get_plugin_version() {
 function get_wpgraphql_content_blocks_plugin_version() {
 	return defined( 'WPGRAPHQL_CONTENT_BLOCKS_VERSION' ) ? WPGRAPHQL_CONTENT_BLOCKS_VERSION : null;
 }
+
+/**
+ * Returns the anonymous client id for this site that has opted in for telemetry
+ */
+function get_telemetry_client_id(): string|null  {
+	/**
+	 * @TODO Upon saving the site's telemetry decision, if they accept, we'll
+	 * also need to generate a unique, anonymous client ID for them to be sent 
+	 * with GA requests.
+	 * 
+	 * If a string is returned, telemetry is enabled and a client id has been generated.
+	 * If this function returns null, either telemetry is off, or a client ID is not created.
+	 */
+ return null;
+}
