4db6714ec83fa52dba47ac0ee81f914d9f01425e
Matthew Wright <matthew.wright@wpengine.com>
2023-12-15 11:09:45 -0600

    updated endpoint and marshall data

--- packages/faustwp-cli/src/telemetry/sendTelemetryData.ts
+++ packages/faustwp-cli/src/telemetry/sendTelemetryData.ts
@@ -1,44 +1,18 @@
 import 'isomorphic-fetch';
 
-import { TelemetryData } from './marshallTelemetryData.js';
 import { getWpUrl } from '../utils/getWpUrl.js';
-import { getWpSecret } from '../utils/getWpSecret.js';
-
-const GA_TRACKING_ID = 'G-KPVSTHK1G4';
-const GA_API_SECRET = '-SLuZb8JTbWkWcT5BD032w';
+import { TelemetryData } from './marshallTelemetryData.js';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const WP_TELEMETRY_ENDPOINT = `${getWpUrl()!}/wp-json/faustwp/v1/telemetry`;
-const WP_SECRET = getWpSecret();
+const WP_TELEMETRY_ENDPOINT = `${getWpUrl()!}/wp-json/faustwp/v1/process_telemetry`;
 
 /**
  * Send the marshalled telemetry data to Faust Telemetry API.
  *
- * @param payload The data being sent to GA
- * @param anonymousId The anonymous ID of the machine we captured during init
+ * @param payload The data being sent to telemetry API
  */
-export const sendTelemetryData = (
-  payload: TelemetryData,
-  anonymousId: string,
-) => {
-  const body = {
-    client_id: anonymousId,
-    wp_secret: WP_SECRET,
-    events: [
-      {
-        name: 'telemetry_event',
-        params: payload,
-      },
-    ],
-  };
-
-  return fetch(
-    `${WP_TELEMETRY_ENDPOINT}?${new URLSearchParams({
-      api_secret: GA_API_SECRET,
-      measurement_id: GA_TRACKING_ID,
-    }).toString()}`,
-    {
-      method: 'POST',
-      body: JSON.stringify(body),
-    },
-  );
+export const sendTelemetryData = (payload: TelemetryData) => {
+  return fetch(WP_TELEMETRY_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  });
 };
