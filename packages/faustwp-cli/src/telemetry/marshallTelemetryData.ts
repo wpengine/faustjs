bb2d45f6dc32fb95afc7850dd8c26711ab04da3a
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2023-12-20 15:25:56 -0600

    MERL-1343/MERL-1342: API rewrite to WP plugin vs GA (#1687)
    * Removed telemetry from the CLI and moved it into the Faust WordPress plugin.
    
    ---------
    
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>
    Co-authored-by: John Parris <john.parris@wpengine.com>

--- packages/faustwp-cli/src/telemetry/marshallTelemetryData.ts
+++ packages/faustwp-cli/src/telemetry/marshallTelemetryData.ts
@@ -1,21 +1,7 @@
 import fs from 'fs';
-import { platform } from 'process';
 import { getCliArgs } from '../utils/index.js';
-import { WPTelemetryResponseData } from './requestWPTelemetryData.js';
 
 export interface TelemetryData {
-  setting_has_frontend_uri?: boolean;
-  setting_redirects_enabled?: boolean;
-  setting_rewrites_enabled?: boolean;
-  setting_themes_disabled?: boolean;
-  setting_img_src_replacement_enabled?: boolean;
-  faustwp_version?: string;
-  wpgraphql_content_blocks_version?: string;
-  is_wpe?: boolean;
-  multisite?: boolean;
-  php_version?: string;
-  wp_version?: string;
-  platform?: string;
   node_faustwp_core_version?: string;
   node_faustwp_cli_version?: string;
   node_faustwp_blocks_version?: string;
@@ -57,13 +43,10 @@ const sanitizePackageJsonVersion = (_version: string | undefined) => {
 };
 
 /**
- * Marshall the JS and WP telemetry data into a single object for GA.
- * @param wpTelemetryData
+ * Marshall the JS telemetry data.
+ * @param command Command that initiated the request
  */
-export const marshallTelemetryData = (
-  wpTelemetryData: WPTelemetryResponseData,
-  command: string,
-): TelemetryData => {
+export const marshallTelemetryData = (command: string) => {
   const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
   const telemetryData: TelemetryData = {
@@ -95,20 +78,6 @@ export const marshallTelemetryData = (
       packageJson?.dependencies?.next as string | undefined,
     ),
     node_is_development: getCliArgs()[0] === 'dev',
-    platform,
-    setting_has_frontend_uri: wpTelemetryData?.faustwp?.has_frontend_uri,
-    setting_redirects_enabled: wpTelemetryData?.faustwp?.redirects_enabled,
-    setting_rewrites_enabled: wpTelemetryData?.faustwp?.rewrites_enabled,
-    setting_themes_disabled: wpTelemetryData?.faustwp?.themes_disabled,
-    setting_img_src_replacement_enabled:
-      wpTelemetryData?.faustwp?.image_source_replacement_enabled,
-    faustwp_version: wpTelemetryData?.faustwp?.version,
-    wpgraphql_content_blocks_version:
-      wpTelemetryData?.wpgraphql_content_blocks?.version,
-    is_wpe: wpTelemetryData?.is_wpe,
-    multisite: wpTelemetryData?.multisite,
-    php_version: wpTelemetryData?.php_version,
-    wp_version: wpTelemetryData?.wp_version,
     command,
   };
 
