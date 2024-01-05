e725bda474216bf3a6adc3ac442055cb91b3765f
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2024-01-05 14:09:41 +0000

    Add phpstan to CI workflow (#1705)
    * build(faustwp): Add phpstan workflow
    
    * Chore: Update composer.lock
    
    * Fix: Fix type error in legacy example
    
    * Fix Codeception e2e test config

---
+++ plugins/faustwp/phpstan/constants.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Constants defined in this file are to help phpstan analyze code where constants outside the plugin (WordPress core constants, etc) are being used.
+ *
+ * @package FaustWP
+ */
+
+define( 'FAUSTWP_FILE', 'faustwp.php' );
+define( 'FAUSTWP_DIR', '' );
+define( 'FAUSTWP_URL', '' );
+define( 'FAUSTWP_PATH', '' );
+define( 'FAUSTWP_SLUG', 'faustwp' );
