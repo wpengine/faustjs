03fb8fb6db94bd5a67127ad93d88d72d8ac93c94
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2024-02-07 13:26:04 -0600

    Fixed inline phpcs error

--- plugins/faustwp/includes/rest/callbacks.php
+++ plugins/faustwp/includes/rest/callbacks.php
@@ -407,7 +407,7 @@ function rest_authorize_permission_callback( \WP_REST_Request $request ) {
 	$secret_key = get_secret_key();
 	$header_key = $request->get_header( 'x-faustwp-secret' );
 
-	// Add console log for get_secret_key()
+	// Add console log for get_secret_key().
 	error_log( 'Secret Key: ' . $secret_key );
 
 	if ( $secret_key && $header_key ) {
