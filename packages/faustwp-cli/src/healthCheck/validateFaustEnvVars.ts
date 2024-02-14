572b2536c47dd56767c38067423c639cb2029080
Blake Wilson <blake.wilson@wpengine.com>
2024-02-14 16:13:10 -0600

    Detect NEXT_PUBLIC_WORDPRESS_URL using wpengine.com TLD and recommend using wpenginepowered.com TLD (#1801)
    * Detect wpengine.com TLD
    
    * Create unit tests
    
    * Update messaging
    
    * Add changeset
    
    * Update packages/faustwp-cli/tests/healthCheck/validateFaustEnvVars.test.ts
    
    Co-authored-by: John Parris <john.parris@wpengine.com>
    
    * Simplify regex test return statement
    
    * Update info message
    
    ---------
    
    Co-authored-by: John Parris <john.parris@wpengine.com>

--- packages/faustwp-cli/src/healthCheck/validateFaustEnvVars.ts
+++ packages/faustwp-cli/src/healthCheck/validateFaustEnvVars.ts
@@ -1,5 +1,11 @@
 import { getWpSecret } from '../utils/index.js';
-import { errorLog, warnLog } from '../stdout/index.js';
+import { errorLog, infoLog, warnLog } from '../stdout/index.js';
+
+export function isWPEngineComSubdomain(url: string) {
+  const regex = /\b\w+\.wpengine\.com\b/;
+
+  return regex.test(url);
+}
 
 /**
  * Validates that the appropriate Faust related environment variables are set.
@@ -9,6 +15,17 @@ export const validateFaustEnvVars = () => {
     errorLog('Could not find NEXT_PUBLIC_WORDPRESS_URL environment variable.');
 
     process.exit(1);
+  }
+
+  if (isWPEngineComSubdomain(process.env.NEXT_PUBLIC_WORDPRESS_URL)) {
+    infoLog(`Found NEXT_PUBLIC_WORDPRESS_URL using wpengine.com TLD.`);
+    infoLog(`It is recommended to use the wpenginepowered.com TLD instead.`);
+    infoLog(
+      `Ex: https://example.wpengine.com -> https://example.wpenginepowered.com`,
+    );
+    infoLog(
+      `This will leverage WP Engine's Advanced Network CDN. See: https://wpengine.com/support/network/`,
+    );
   }
 
   if (!getWpSecret()) {
