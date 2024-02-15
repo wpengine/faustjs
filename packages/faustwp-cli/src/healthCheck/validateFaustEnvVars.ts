e57516b071bb857131d744ec1309696774813ba7
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2024-02-15 12:03:35 -0600

    Refactored getWpSecret to const secretWp

--- packages/faustwp-cli/src/healthCheck/validateFaustEnvVars.ts
+++ packages/faustwp-cli/src/healthCheck/validateFaustEnvVars.ts
@@ -11,6 +11,8 @@ export function isWPEngineComSubdomain(url: string) {
  * Validates that the appropriate Faust related environment variables are set.
  */
 export const validateFaustEnvVars = async () => {
+  const secretWp = getWpSecret();
+
   if (!process.env.NEXT_PUBLIC_WORDPRESS_URL) {
     errorLog('Could not find NEXT_PUBLIC_WORDPRESS_URL environment variable.');
 
@@ -28,15 +30,12 @@ export const validateFaustEnvVars = async () => {
     );
   }
 
-  if (!getWpSecret()) {
+  if (!secretWp) {
     warnLog('Could not find FAUST_SECRET_KEY environment variable.');
     warnLog('Some functionality may be limited.');
   }
 
-  if (
-    process.env.NEXT_PUBLIC_WORDPRESS_URL.startsWith('http://') &&
-    getWpSecret()
-  ) {
+  if (process.env.NEXT_PUBLIC_WORDPRESS_URL.startsWith('http://') && secretWp) {
     warnLog('Your WordPress site is not running on https!');
     warnLog(
       'This is a security concern as all traffic with your secret key is in plain text.',
@@ -45,20 +44,19 @@ export const validateFaustEnvVars = async () => {
       'Please make sure your production Faust app runs with a WordPress instance on https!',
     );
   }
-  if (getWpSecret()) {
+
+  if (secretWp) {
     // send secret key
     const apiUrl = `${getWpUrl()}/?rest_route=/faustwp/v1/validate_secret_key`;
     const headers = {
-      'x-faustwp-secret': getWpSecret(),
+      'x-faustwp-secret': secretWp,
     };
     try {
       const response = await fetch(apiUrl, {
         headers,
         method: 'POST',
       });
-      if (response.status === 204) {
-        // Success: User receives a 204 status code
-      } else if (response.status === 401) {
+      if (response.status === 401) {
         // Unauthorized: User receives a 401 status code AND the message below
         errorLog(
           'Ensure your FAUST_SECRET_KEY environment variable matches your Secret Key in the Faust WordPress plugin settings',
