259d926398855f915e0880bbee631f51836ddd86
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2024-03-01 15:32:32 +0000

    Add next-secure-headers in example projects (#1803)
    * Add next-secure-headers in example projects
    
    * Update next.config.js paths
    
    * FeatI: set xssProtection: false

--- examples/next/block-support/next.config.js
+++ examples/next/block-support/next.config.js
@@ -1,4 +1,5 @@
 const { withFaust, getWpHostname } = require('@faustwp/core');
+const { createSecureHeaders } = require('next-secure-headers');
 
 /**
  * @type {import('next').NextConfig}
@@ -14,5 +15,10 @@ module.exports = withFaust({
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
+  },
+  async headers() {
+    return [{ source: '/:path*', headers: createSecureHeaders({
+      xssProtection: false
+    }) }];
   },
 });
