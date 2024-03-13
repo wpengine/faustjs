d64a3feb253ef824240d5b0aa73c4d98c967212a
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2024-03-12 22:05:01 -0500

    Expose avatar in viewer query when authenticated

--- packages/faustwp-core/src/hooks/useAuth.tsx
+++ packages/faustwp-core/src/hooks/useAuth.tsx
@@ -39,6 +39,9 @@ type ViewerType = {
   uri?: string;
   url?: string;
   userId?: number;
+  avatar?: {
+    url: string;
+  };
 };
 
 export type UseAuthConfig = RedirectStrategyConfig | LocalStrategyConfig;
@@ -173,6 +176,11 @@ export function useAuth(_config?: UseAuthConfig) {
               uri
               url
               userId
+              avatar {
+                forceDefault
+                isRestricted
+                url
+              }
             }
           }
         `,
