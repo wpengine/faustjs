48479940e7a21d83f07c355f1b98ecd64cde47c2
Jason Bahl <jasonbahl@mac.com>
2024-09-05 16:52:42 -0600

    - update block-editor-utils moduleNameMapper as it was causing issues with jest

--- packages/block-editor-utils/jest.config.js
+++ packages/block-editor-utils/jest.config.js
@@ -28,9 +28,6 @@ module.exports = {
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-    "uuid": require.resolve('uuid'),
-    "^react($|/.+)": "<rootDir>/node_modules/react$1",
-    '^react-dom($|/.+)': '<rootDir>/node_modules/react-dom$1'
   },
   collectCoverage: true,
   coverageReporters: ['json', 'html'],
