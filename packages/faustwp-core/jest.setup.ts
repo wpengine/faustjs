77ce5c846ad6b7a52f68e8a3a86b2ca6a4d17d32
Jason Bahl <jasonbahl@mac.com>
2024-09-05 16:54:47 -0600

    - mock useRouter in jest to satisfy next 13 requirements

--- packages/faustwp-core/jest.setup.ts
+++ packages/faustwp-core/jest.setup.ts
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom'; // For custom matchers. See https://github.com/testing-library/jest-dom#custom-matchers.
+
+import { jest } from '@jest/globals';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      prefetch: jest.fn().mockResolvedValue(undefined),
+      beforePopState: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+    };
+  },
+}));
