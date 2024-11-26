57d876190303339cd633e4c19a8088ed5b642d42
Cesar Benavides <cesarbenavides@icloud.com>
2024-11-26 12:24:59 -0600

    fix(experimental-app-router): Resolve onLogin issue on Vercel deployment - Adding use server on logout action(#1919)

--- packages/experimental-app-router/src/server-actions/logoutAction.ts
+++ packages/experimental-app-router/src/server-actions/logoutAction.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers.js';
 import { getWpUrl } from '../faust-core-utils.js';
 
 export async function onLogout() {
-  ('use server');
+  'use server';
 
   const wpCookieName = `${getWpUrl()}-rt`;
   // eslint-disable-next-line @typescript-eslint/await-thenable
