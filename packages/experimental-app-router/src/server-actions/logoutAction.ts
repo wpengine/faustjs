cf59db01047d3e4f7a65c235455fc3626bfc91a6
Cesar Benavides <cesarbenavides@icloud.com>
2024-11-26 12:01:55 -0600

    fix(experimental-app-router): Resolve onLogin issue on Vercel deployment - adding one more eslint addition (#1919)

--- packages/experimental-app-router/src/server-actions/logoutAction.ts
+++ packages/experimental-app-router/src/server-actions/logoutAction.ts
@@ -2,9 +2,10 @@ import { cookies } from 'next/headers.js';
 import { getWpUrl } from '../faust-core-utils.js';
 
 export async function onLogout() {
-  'use server';
+  ('use server');
 
   const wpCookieName = `${getWpUrl()}-rt`;
+  // eslint-disable-next-line @typescript-eslint/await-thenable
   const cookieStore = await cookies();
   const wpCookie = cookieStore.get(wpCookieName);
 
