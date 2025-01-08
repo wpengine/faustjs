c6fcb9144d80af34deb38bf913f8031801d8e8f7
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-01-08 12:51:39 +0000

    Reverted test deletion.

--- packages/blocks/tests/components/WordPressBlocksProvider.test.tsx
+++ packages/blocks/tests/components/WordPressBlocksProvider.test.tsx
@@ -11,6 +11,13 @@ import type { BlocksTheme } from '../../src/types/theme';
 import { renderHook } from '@testing-library/react';  // Import from @testing-library/react
 
 describe('useBlocksTheme', () => {
+  it('Throws an error if not used within WordPressBlocksProvider', () => {
+    // Assert that renderHook throws an error when used outside of WordPressBlocksProvider
+    expect(() => {
+      renderHook(() => useBlocksTheme());
+    }).toThrow('useBlocksTheme hook was called outside of context, make sure your app is wrapped with WordPressBlocksProvider');
+  });
+
   it('returns the passed in theme from WordPressBlocksProvider', () => {
     // Wrapping component to provide context
     const wrapper = ({ children }: PropsWithChildren<{}>) => {
