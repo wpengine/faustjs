cd0fd2572a7c6d4a106334e02bb07cdfa43525bf
Kellen Mace <kellen.mace@gmail.com>
2025-02-14 13:36:53 -0500

    Remove unnecessary content locations from Tailwind config

--- examples/next/tutorial/tailwind.config.mjs
+++ examples/next/tutorial/tailwind.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
