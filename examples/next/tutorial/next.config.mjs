0657849fc27f4b69f6845ca85f694da1daa905b4
Kellen Mace <kellen.mace@gmail.com>
2025-02-18 18:34:30 -0500

    Add secure headers to example app

--- examples/next/tutorial/next.config.mjs
+++ examples/next/tutorial/next.config.mjs
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const { createSecureHeaders } = require("next-secure-headers");
 
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = withFaust({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: ["node_modules"],
+  },
+  images: {
+    domains: [getWpHostname()],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: createSecureHeaders({
+          xssProtection: false,
+        }),
+      },
+    ];
+  },
+});
