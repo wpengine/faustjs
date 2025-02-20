694446624cba550d4b81926ee11eb8c87b1bb23d
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-02-20 16:08:48 +0000

    PHPCS fix.

--- plugins/faustwp/faustwp.php
+++ plugins/faustwp/faustwp.php
@@ -61,7 +61,7 @@ if ( ! is_php_version_compatible( faustwp_minimum_php_requirement() ) ) {
 	return;
 }
 
-// Loads the updater service, if included in this build
+// Loads the updater service, if included in this build.
 if ( file_exists( FAUSTWP_DIR . '/includes/updates/class-plugin-updater.php' ) ) {
 	require FAUSTWP_DIR . '/includes/updates/class-plugin-updater.php';
 }
