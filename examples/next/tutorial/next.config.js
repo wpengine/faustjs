cf29b2e2899ffb4c728b4d9c83979b9a3dcc7772
Kellen Mace <kellen.mace@gmail.com>
2025-03-11 19:30:02 -0400

    Add missing imports to next config file

--- examples/next/tutorial/next.config.js
+++ examples/next/tutorial/next.config.js
@@ -1,25 +1,25 @@
+const { withFaust, getWpHostname } = require('@faustwp/core');
 const { createSecureHeaders } = require('next-secure-headers');
 
 /**
  * @type {import('next').NextConfig}
  **/
 module.exports = withFaust({
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['node_modules'],
-  },
-  images: {
-    domains: [getWpHostname()],
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: createSecureHeaders({
-          xssProtection: false,
-        }),
-      },
-    ];
-  },
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: ['node_modules'],
+	},
+	images: {
+		domains: [getWpHostname()],
+	},
+	async headers() {
+		return [
+			{
+				source: '/:path*',
+				headers: createSecureHeaders({
+					xssProtection: false,
+				}),
+			},
+		];
+	},
 });
