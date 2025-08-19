fdb6f0c93cc28cf4c62d04487fa2f44c4884efef
ahuseyn <huseyn.aghayev@wpengine.com>
2025-08-19 17:18:43 +0200

    style fixes

--- examples/nextjs/previews/src/components/Header.js
+++ examples/nextjs/previews/src/components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Header() {
 	return (
-		<header className="bg-gray-800 text-white py-4 px-8 mb-8">
+		<header className="bg-gray-800 text-white py-4 px-8">
 			<div className="flex justify-between items-center max-w-4xl mx-auto">
 				<div className="text-3xl font-semibold">
 					<Link href="/">Headless</Link>
--- examples/nextjs/previews/src/components/Layout.js
+++ examples/nextjs/previews/src/components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
 	return (
 		<>
 			<Header />
-			<main className="bg-stone-100 text-gray-800 pb-16 min-h-screen">
+			<main className="bg-stone-100 text-gray-800 pb-16 pt-8 min-h-screen">
 				{children}
 
 				{router.isPreview && <PreviewButton />}
