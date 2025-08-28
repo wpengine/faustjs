6e6365e3c5afdb36318c5eccd38c73c8010ee754
ahuseyn <huseyn.aghayev@wpengine.com>
2025-08-28 16:00:53 +0200

    add preview

--- examples/sveltekit/kitchen-sink/src/routes/[...identifier]/+page.server.js
+++ examples/sveltekit/kitchen-sink/src/routes/[...identifier]/+page.server.js
@@ -4,24 +4,41 @@ import {
 	uriToTemplate,
 } from '@faustjs/sveltekit';
 import { WORDPRESS_URL } from '$env/static/private';
+import { WP_PREVIEW_SECRET } from '$env/static/private';
 import availableQueries from '../../queries/templateQueries/index.js';
 import { fetchTemplateQueries } from '@faustjs/data-fetching';
+import { getAuthString } from '../../utils/getAuthString.js';
 
 export const load = async (event) => {
 	const {
-		params: { uri },
+		params: { identifier },
+		url,
 		fetch,
 	} = event;
 
-	const workingUri = uri || '/';
+	const searchParams = url?.searchParams;
 
-	const client = createDefaultClient(WORDPRESS_URL);
+	// Determine if we are in preview mode based on the URL parameter and the secret
+	const isPreview =
+		searchParams.get('preview') === 'true' &&
+		WP_PREVIEW_SECRET === searchParams.get('secret');
+
+	const headers = isPreview ? { Authorization: getAuthString() } : undefined;
+
+	const client = createDefaultClient(WORDPRESS_URL, headers);
 	setGraphQLClient(client);
+
+	const variables = isPreview
+		? {
+				id: identifier,
+				asPreview: true,
+		  }
+		: { uri: identifier || '/' };
 
 	const templateData = await uriToTemplate({
 		fetch,
-		uri: workingUri,
 		graphqlClient: client,
+		...variables,
 	});
 
 	// Fetch template-specific queries using the same mechanism as Next.js
@@ -40,7 +57,6 @@ export const load = async (event) => {
 	}
 
 	return {
-		uri: workingUri,
 		templateData,
 		queriesData,
 	};
