696d38c590536554f5703d7046003a22f0ec5b46
ahuseyn <huseyn.aghayev@wpengine.com>
2025-10-07 12:45:42 +0200

    suggested fixes, remove isValidUrl and isValidEmail, add package version

--- packages/utilities/src/assert.ts
+++ packages/utilities/src/assert.ts
@@ -36,32 +36,3 @@ export function isPreviewPath(uri: string): boolean {
 
 	return previewRegex.test(uri);
 }
-
-/**
- * Returns whether or not a string is a valid URL
- *
- * @export
- * @returns
- */
-export function isValidUrl(url: string): boolean {
-	try {
-		// eslint-disable-next-line no-new
-		new URL(url);
-		return true;
-	} catch (e) {
-		return false;
-	}
-}
-
-export const emailRegex =
-	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line no-useless-escape
-
-/**
- * Returns whether or not a string is a valid email address
- *
- * @export
- * @returns
- */
-export function isValidEmail(email: string): boolean {
-	return emailRegex.test(String(email).toLowerCase());
-}
