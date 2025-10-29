5f3a9ed14d1945031232dc1b02b21fa5eaa6e2ea
ahuseyn <huseyn.aghayev@wpengine.com>
2025-10-29 12:07:09 +0100

    fix(cli): improve validateNextWordPressUrl error messages

---
+++ .changeset/salty-files-build.md
@@ -0,0 +1,5 @@
+---
+'@faustwp/cli': patch
+---
+
+Improve validateNextWordPressUrl error messages by including HTTP status code and status text, also providing an additional clarity for fatal errors.
--- packages/faustwp-cli/src/healthCheck/validateNextWordPressUrl.ts
+++ packages/faustwp-cli/src/healthCheck/validateNextWordPressUrl.ts
@@ -5,6 +5,9 @@ import { errorLog, warnLog } from '../stdout/index.js';
  * Validates the NEXT_PUBLIC_WORDPRESS_URL environment variable by sending a POST request to the Faust Plugin API.
  * If the URL matches the Faust Plugin Headless URL, the validation fails, and an error is logged.
  */
+
+const FAILED_VALIDATION_MESSAGE = 'Validation Failed, Faust is shutting down:';
+
 export async function validateNextWordPressUrl(): Promise<void> {
 	const apiUrl = `${getWpUrl()}/wp-json/faustwp/v1/validate_public_wordpress_url`;
 	const headers = {
@@ -23,6 +26,13 @@ export async function validateNextWordPressUrl(): Promise<void> {
 		});
 
 		if (!response.ok) {
+			errorLog(
+				'Validation error:',
+				String(response.status),
+				'-',
+				response.statusText,
+			);
+
 			if (response.status === 404) {
 				// Handle the case when the route does not exist
 				warnLog(
@@ -31,11 +41,14 @@ export async function validateNextWordPressUrl(): Promise<void> {
 			} else if (response.status >= 500 && response.status < 600) {
 				// Handle WordPress server error
 				errorLog(
+					FAILED_VALIDATION_MESSAGE,
 					'Could not connect to the WordPress server. Please check your WordPress URL or server status, as your site may not function correctly.',
 				);
+				process.exit(1);
 			} else {
 				errorLog(
-					'Validation Failed: Your Faust front-end site URL value is misconfigured. It should NOT match the `NEXT_PUBLIC_WORDPRESS_URL.`',
+					FAILED_VALIDATION_MESSAGE,
+					'Your Faust front-end site URL value is misconfigured. It should NOT match the `NEXT_PUBLIC_WORDPRESS_URL.`',
 				);
 				process.exit(1);
 			}
