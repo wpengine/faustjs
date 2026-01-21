7d2e095cf5d137b45e61f49e92b91e504c14c123
Alex Moon <alex.jared.moon@gmail.com>
2026-01-21 15:21:27 -0800

    fix: example image issue

--- examples/next/faustwp-getting-started/components/FeaturedImage/FeaturedImage.js
+++ examples/next/faustwp-getting-started/components/FeaturedImage/FeaturedImage.js
@@ -1,58 +1,61 @@
 import { gql } from '@apollo/client';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 export default function FeaturedImage({
-  image,
-  width,
-  height,
-  className,
-  priority,
-  layout,
-  ...props
+	image,
+	width,
+	height,
+	className,
+	priority,
+	layout,
+	...props
 }) {
-  const src = image?.sourceUrl;
+	const src = image?.sourceUrl;
 
-  if (!src) return null;
+	if (!src) return null;
 
-  const { altText = '', mediaDetails = {} } = image ?? {};
+	const { altText = '', mediaDetails = {} } = image ?? {};
 
-  layout = layout ?? 'fill';
+	layout = layout ?? 'fill';
 
-  const dimensions = {
-    width: layout === 'fill' ? undefined : width ?? mediaDetails?.width,
-    height: layout === 'fill' ? undefined : height ?? mediaDetails?.height
-  };
+	const dimensions = {
+		width: layout === 'fill' ? undefined : width ?? mediaDetails?.width,
+		height: layout === 'fill' ? undefined : height ?? mediaDetails?.height,
+	};
 
-  if (layout !== 'fill' && (!dimensions.width || !dimensions.height)) return null;
+	if (layout !== 'fill' && (!dimensions.width || !dimensions.height))
+		return null;
 
-  return (
-    <figure className={className}>
-      <Image
-        src={src}
-        alt={altText}
-        fill={layout}
-        width={dimensions.width}
-        height={dimensions.height}
-        priority={priority}
-        {...props}
-      />
-    </figure>
-  );
+	console.log(layout, mediaDetails, dimensions);
+
+	return (
+		<figure className={className}>
+			<Image
+				src={src}
+				alt={altText}
+				layout={layout}
+				width={dimensions.width}
+				height={dimensions.height}
+				priority={priority}
+				{...props}
+			/>
+		</figure>
+	);
 }
 
 FeaturedImage.fragments = {
-  entry: gql`
-    fragment FeaturedImageFragment on NodeWithFeaturedImage {
-      featuredImage {
-        node {
-          id
-          sourceUrl
-          altText
-          mediaDetails {
-            width
-            height
-          }
-        }
-      }
-    }
-  `,
+	entry: gql`
+		fragment FeaturedImageFragment on NodeWithFeaturedImage {
+			featuredImage {
+				node {
+					id
+					sourceUrl
+					altText
+					mediaDetails {
+						width
+						height
+					}
+				}
+			}
+		}
+	`,
 };
