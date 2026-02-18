3e372655c506b03ff2b62e7cec9359d0ef3454b7
ahuseyn <huseyn.aghayev@wpengine.com>
2026-02-18 17:24:36 +0100

    implement multiple queries into starter example

--- examples/next/faustwp-getting-started/wp-templates/page.js
+++ examples/next/faustwp-getting-started/wp-templates/page.js
@@ -1,91 +1,82 @@
 import { gql } from '@apollo/client';
+import { useFaustQuery } from '@faustwp/core';
 import * as MENUS from '../constants/menus';
-import { BlogInfoFragment } from '../fragments/GeneralSettings';
 import {
-  Header,
-  Footer,
-  Main,
-  Container,
-  ContentWrapper,
-  EntryHeader,
-  NavigationMenu,
-  FeaturedImage,
-  SEO,
+	Header,
+	Footer,
+	Main,
+	Container,
+	ContentWrapper,
+	EntryHeader,
+	FeaturedImage,
+	SEO,
 } from '../components';
+import { GET_LAYOUT_QUERY } from '../fragments/LayoutQuery';
+
+const GET_PAGE_QUERY = gql`
+	${FeaturedImage.fragments.entry}
+	query GetPage($databaseId: ID!, $asPreview: Boolean = false) {
+		page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
+			title
+			content
+			...FeaturedImageFragment
+		}
+	}
+`;
 
 export default function Component(props) {
-  // Loading state for previews
-  if (props.loading) {
-    return <>Loading...</>;
-  }
+	// Loading state for previews
+	if (props.loading) {
+		return <>Loading...</>;
+	}
 
-  const { title: siteTitle, description: siteDescription } =
-    props?.data?.generalSettings;
-  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
-  const { title, content, featuredImage } = props?.data?.page ?? { title: '' };
+	const { generalSettings, headerMenuItems, footerMenuItems } =
+		useFaustQuery(GET_LAYOUT_QUERY);
+	const { page } = useFaustQuery(GET_PAGE_QUERY);
 
-  return (
-    <>
-      <SEO
-        title={siteTitle}
-        description={siteDescription}
-        imageUrl={featuredImage?.node?.sourceUrl}
-      />
-      <Header
-        title={siteTitle}
-        description={siteDescription}
-        menuItems={primaryMenu}
-      />
-      <Main>
-        <>
-          <EntryHeader title={title} image={featuredImage?.node} />
-          <Container>
-            <ContentWrapper content={content} />
-          </Container>
-        </>
-      </Main>
-      <Footer title={siteTitle} menuItems={footerMenu} />
-    </>
-  );
+	const { title: siteTitle, description: siteDescription } = generalSettings;
+	const primaryMenu = headerMenuItems?.nodes ?? [];
+	const footerMenu = footerMenuItems?.nodes ?? [];
+	const { title, content, featuredImage } = page ?? { title: '' };
+
+	return (
+		<>
+			<SEO
+				title={siteTitle}
+				description={siteDescription}
+				imageUrl={featuredImage?.node?.sourceUrl}
+			/>
+			<Header
+				title={siteTitle}
+				description={siteDescription}
+				menuItems={primaryMenu}
+			/>
+			<Main>
+				<>
+					<EntryHeader title={title} image={featuredImage?.node} />
+					<Container>
+						<ContentWrapper content={content} />
+					</Container>
+				</>
+			</Main>
+			<Footer title={siteTitle} menuItems={footerMenu} />
+		</>
+	);
 }
 
-Component.variables = ({ databaseId }, ctx) => {
-  return {
-    databaseId,
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-    asPreview: ctx?.asPreview,
-  };
-};
-
-Component.query = gql`
-  ${BlogInfoFragment}
-  ${NavigationMenu.fragments.entry}
-  ${FeaturedImage.fragments.entry}
-  query GetPageData(
-    $databaseId: ID!
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
-    $asPreview: Boolean = false
-  ) {
-    page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-      title
-      content
-      ...FeaturedImageFragment
-    }
-    generalSettings {
-      ...BlogInfoFragment
-    }
-    footerMenuItems: menuItems(where: { location: $footerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-  }
-`;
+Component.queries = [
+	{
+		query: GET_LAYOUT_QUERY,
+		variables: (seedNode, ctx) => ({
+			headerLocation: MENUS.PRIMARY_LOCATION,
+			footerLocation: MENUS.FOOTER_LOCATION,
+		}),
+	},
+	{
+		query: GET_PAGE_QUERY,
+		variables: ({ databaseId }, ctx) => ({
+			databaseId,
+			asPreview: ctx?.asPreview,
+		}),
+	},
+];
--- examples/next/faustwp-getting-started/wp-templates/tag.js
+++ examples/next/faustwp-getting-started/wp-templates/tag.js
@@ -1,106 +1,98 @@
 import { gql } from '@apollo/client';
+import { useFaustQuery } from '@faustwp/core';
 import * as MENUS from '../constants/menus';
-import { BlogInfoFragment } from '../fragments/GeneralSettings';
 import {
-  Header,
-  Footer,
-  Main,
-  Container,
-  EntryHeader,
-  NavigationMenu,
-  FeaturedImage,
-  Post,
-  SEO,
+	Header,
+	Footer,
+	Main,
+	Container,
+	EntryHeader,
+	FeaturedImage,
+	Post,
+	SEO,
 } from '../components';
+import { GET_LAYOUT_QUERY } from '../fragments/LayoutQuery';
 
-export default function Component(props) {
-  const { title: siteTitle, description: siteDescription } =
-    props?.data?.generalSettings;
-  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
-  const { name, posts } = props.data.nodeByUri;
-
-  return (
-    <>
-      <SEO title={siteTitle} description={siteDescription} />
-      <Header
-        title={siteTitle}
-        description={siteDescription}
-        menuItems={primaryMenu}
-      />
-      <Main>
-        <>
-          <EntryHeader title={`Tag: ${name}`} />
-          <Container>
-            {posts.edges.map((post) => (
-              <Post
-                title={post.node.title}
-                content={post.node.content}
-                date={post.node.date}
-                author={post.node.author?.node.name}
-                uri={post.node.uri}
-                featuredImage={post.node.featuredImage?.node}
-              />
-            ))}
-          </Container>
-        </>
-      </Main>
-      <Footer title={siteTitle} menuItems={footerMenu} />
-    </>
-  );
-}
-
-Component.query = gql`
-  ${BlogInfoFragment}
-  ${NavigationMenu.fragments.entry}
-  ${FeaturedImage.fragments.entry}
-  query GetTagPage(
-    $uri: String!
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
-  ) {
-    nodeByUri(uri: $uri) {
-      ... on Tag {
-        name
-        posts {
-          edges {
-            node {
-              id
-              title
-              content
-              date
-              uri
-              ...FeaturedImageFragment
-              author {
-                node {
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    generalSettings {
-      ...BlogInfoFragment
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-    footerMenuItems: menuItems(where: { location: $footerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-  }
+const GET_TAG_QUERY = gql`
+	${FeaturedImage.fragments.entry}
+	query GetTag($uri: String!) {
+		nodeByUri(uri: $uri) {
+			... on Tag {
+				name
+				posts {
+					edges {
+						node {
+							id
+							title
+							content
+							date
+							uri
+							...FeaturedImageFragment
+							author {
+								node {
+									name
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 `;
 
-Component.variables = ({ uri }) => {
-  return {
-    uri,
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-  };
-};
+export default function Component(props) {
+	const { generalSettings, headerMenuItems, footerMenuItems } =
+		useFaustQuery(GET_LAYOUT_QUERY);
+	const { nodeByUri } = useFaustQuery(GET_TAG_QUERY);
+
+	const { title: siteTitle, description: siteDescription } = generalSettings;
+	const primaryMenu = headerMenuItems?.nodes ?? [];
+	const footerMenu = footerMenuItems?.nodes ?? [];
+	const { name, posts } = nodeByUri;
+
+	return (
+		<>
+			<SEO title={siteTitle} description={siteDescription} />
+			<Header
+				title={siteTitle}
+				description={siteDescription}
+				menuItems={primaryMenu}
+			/>
+			<Main>
+				<>
+					<EntryHeader title={`Tag: ${name}`} />
+					<Container>
+						{posts.edges.map((post) => (
+							<Post
+								title={post.node.title}
+								content={post.node.content}
+								date={post.node.date}
+								author={post.node.author?.node.name}
+								uri={post.node.uri}
+								featuredImage={post.node.featuredImage?.node}
+							/>
+						))}
+					</Container>
+				</>
+			</Main>
+			<Footer title={siteTitle} menuItems={footerMenu} />
+		</>
+	);
+}
+
+Component.queries = [
+	{
+		query: GET_LAYOUT_QUERY,
+		variables: (seedNode, ctx) => ({
+			headerLocation: MENUS.PRIMARY_LOCATION,
+			footerLocation: MENUS.FOOTER_LOCATION,
+		}),
+	},
+	{
+		query: GET_TAG_QUERY,
+		variables: ({ uri }, ctx) => ({
+			uri,
+		}),
+	},
+];
